a334ee4c5d4cc1e86bd783ea600cb8c6e5961967
Brigs <abrignoni@gmail.com>
2019-12-30 22:21:42 -0500

    Update ileapp.py

--- ileapp.py
+++ ileapp.py
@@ -24,13 +24,11 @@ args = parser.parse_args()
 pathto = args.pathtodir
 extracttype = args.o
 start = process_time()
-'''	
+
 tosearch = {'mib':'*mobile_installation.log.*', 'iconstate':'*SpringBoard/IconState.plist', 'lastbuild':'*LastBuildInfo.plist', 'iOSNotifications11':'*PushStore*', 'iOSNotifications12':'*private/var/mobile/Library/UserNotifications*',
 		'wireless':'*wireless/Library/Preferences/com.apple.*','knowledgec':'*CoreDuet/Knowledge/knowledgeC.db','applicationstate':'*pplicationState.db*', 'conndevices':'*/iTunes_Control/iTunes/iTunesPrefs', 'ktx':'*.ktx*'}
 
-'''	
-tosearch = {'ktx':'*@3x.ktx*'}
-'''	
+
 
 	
 os.makedirs(reportfolderbase)
