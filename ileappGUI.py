034767539ff91e7d70864680ee4d46f7d069e305
Brigs <abrignoni@gmail.com>
2020-02-18 22:23:30 -0500

    Update ileappGUI.py

--- ileappGUI.py
+++ ileappGUI.py
@@ -104,7 +104,7 @@ while True:
 				'wapcontact':'*ContactsV2.sqlite',
 				'actrec':'*activation_record.plist',
 				'DHCPhp':'*private/var/db/dhcpd_leases*',
-				'DHCPL':'*private/var/db/dhcpclient/leases/*',
+				'DHCPL':'*private/var/db/dhcpclient/leases/en*',
 				'redditusers':'*Data/Application/*/Documents/*/accounts/*',
 				'redditchats':'*Data/Application/*/Documents/*/accountData/*/chat/*/chat.sqlite',
 				'interactionc':'*interactionC.db'}
