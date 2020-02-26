180d47e58ffc6be3c6e620520d13b42259ebe0f7
Agam Dua <agam.dua@gc.com>
2020-02-26 13:46:54 -0500

    Ran black formatting

--- search_files.py
+++ search_files.py
@@ -8,28 +8,30 @@ from ilapfuncs import *
 
 
 def search(pathto, filename):
-	return [ fname for fname in Path(pathto).rglob(filename) ]
+    return [fname for fname in Path(pathto).rglob(filename)]
+
 
 def searchtar(t, val, reportfolderbase):
-	temp = reportfolderbase+'temp/'
-	pathlist = []
-	for member in t.getmembers():
-		if fnmatch.fnmatch(member.name, val):
-			try:
-				t.extract(member.name, path=temp)
-				pathlist.append(temp+member.name)
-			except:
-				logfunc('Could not write file to filesystem')
-	return pathlist
+    temp = reportfolderbase + "temp/"
+    pathlist = []
+    for member in t.getmembers():
+        if fnmatch.fnmatch(member.name, val):
+            try:
+                t.extract(member.name, path=temp)
+                pathlist.append(temp + member.name)
+            except:
+                logfunc("Could not write file to filesystem")
+    return pathlist
+
 
 def searchzip(z, name_list, val, reportfolderbase):
-	temp = reportfolderbase+'temp'
-	pathlist = []
-	for member in name_list:
-		if fnmatch.fnmatch(member, val):
-			try:
-				z.extract(member, path=temp)
-				pathlist.append(temp+member)
-			except:
-				logfunc('Could not write file to filesystem')	
-	return pathlist
+    temp = reportfolderbase + "temp"
+    pathlist = []
+    for member in name_list:
+        if fnmatch.fnmatch(member, val):
+            try:
+                z.extract(member, path=temp)
+                pathlist.append(temp + member)
+            except:
+                logfunc("Could not write file to filesystem")
+    return pathlist
