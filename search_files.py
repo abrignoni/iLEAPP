695faf188f88424abd841f8d25de2baa0dc13da0
Agam Dua <agam@comfylabs.io>
2020-03-01 20:54:53 -0500

    Applied: formatting changes with the 'make format' cmd

--- search_files.py
+++ search_files.py
@@ -12,7 +12,7 @@ def search(pathto, filename):
 
 
 def searchtar(t, val, reportfolderbase):
-    temp = os.path.join(reportfolderbase, 'temp')
+    temp = os.path.join(reportfolderbase, "temp")
     pathlist = []
     for member in t.getmembers():
         if fnmatch.fnmatch(member.name, val):
@@ -20,17 +20,18 @@ def searchtar(t, val, reportfolderbase):
                 t.extract(member.name, path=temp)
                 pathlist.append(os.path.join(temp, Path(member.name)))
             except:
-                logfunc('Could not write file to filesystem')
+                logfunc("Could not write file to filesystem")
     return pathlist
 
+
 def searchzip(z, name_list, val, reportfolderbase):
-    temp = os.path.join(reportfolderbase, 'temp')
+    temp = os.path.join(reportfolderbase, "temp")
     pathlist = []
     for member in name_list:
         if fnmatch.fnmatch(member, val):
             try:
                 z.extract(member, path=temp)
-                pathlist.append(temp+member)
+                pathlist.append(temp + member)
             except:
-                logfunc('Could not write file to filesystem') 
+                logfunc("Could not write file to filesystem")
     return pathlist
