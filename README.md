10d597ff075f7ac0c16439b7c1c7be1011cc3e9c
Agam Dua <agam@comfylabs.io>
2020-03-04 16:08:13 -0500

    Added: docs about dir structure
    and also a make target for creating new plugins.

--- README.md
+++ README.md
@@ -55,3 +55,63 @@ $ python ileappGUI.py
 ```
 
 The GUI will open in another window.
+
+
+## Development
+
+### Development environment
+
+The repository uses Python 3, and expects that you have a `virtualenv` set up.
+
+Furthermore, development has solely been done on macOS so far, so if there are
+issues, please log them on the [bug
+tracker](https://github.com/abrignoni/iLEAPP/issues).
+
+### Navigating the code
+
+`./ileapp.py` and `./ileappGUI.py` are the entry points for each type of program,
+the former being for the CLI and latter for the GUI.
+
+The actual business logic of the code is split between `./ilapfuncs.py` and in
+`contrib/`. The goal is to port all plugin code to `contrib/` and source from
+there.
+
+In the current model, all plugins ported to `contrib/` are imported to
+the `ilapfuncs` module so the CLI/GUI frontends do not need to be refactored
+immediately.
+
+The directory structure of an example plugin which reads from sqlite and writes
+an HTML template at the end is as follows:
+
+```
+$ tree contrib/aggregated_dictionary/
+contrib/aggregated_dictionary/
+├── __init__.py
+├── html
+│   └── passcode_type.html
+├── main.py
+└── sql.py
+```
+
+In this, the business logic is in `main.py`, any sql queries are sourced from
+`sql.py` and all HTML is saved as `jinja2` templates in `html/`.
+
+### Adding a new plugin to `contrib/`
+
+**NOTE:** Currently external plugins are not supported, they must be merged into iLEAPP
+into `contrib/`.
+
+To add a new plugin, some scaffolding code has been provided. 
+
+One can run:
+
+```
+make add_plugin name=the_name_of_your_plugin
+```
+
+After this, one still needs to import the plugin to `ilapfuncs`:
+
+```python
+# in ilepfuncs.py
+from contrib.the_name_of_your_plugin import function_name
+```
---
+++ contrib/accounts/main.py
@@ -0,0 +1,91 @@
+def accs(filefound):
+    try:
+        db = sqlite3.connect(filefound[0])
+        cursor = db.cursor()
+        cursor.execute(
+            """
+		SELECT
+		ZACCOUNTTYPEDESCRIPTION,
+		ZUSERNAME,
+		DATETIME(ZDATE+978307200,'UNIXEPOCH','UTC' ) AS 'ZDATE TIMESTAMP',
+		ZACCOUNTDESCRIPTION,
+		ZACCOUNT.ZIDENTIFIER,
+		ZACCOUNT.ZOWNINGBUNDLEID
+		FROM ZACCOUNT
+		JOIN ZACCOUNTTYPE ON ZACCOUNTTYPE.Z_PK=ZACCOUNT.ZACCOUNTTYPE
+		ORDER BY ZACCOUNTTYPEDESCRIPTION
+		"""
+        )
+
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            logfunc(f"Account Data function executing")
+
+            if os.path.isdir(os.path.join(reportfolderbase, "Accounts/")):
+                pass
+            else:
+                os.makedirs(os.path.join(reportfolderbase, "Accounts"))
+
+            with open(
+                os.path.join(reportfolderbase, "Accounts/Accounts.html"),
+                "w",
+                encoding="utf8",
+            ) as f:
+                f.write("<html><body>")
+                f.write("<h2> Account Data report</h2>")
+                f.write(f"Account Data entries: {usageentries}<br>")
+                f.write(f"Account Data located at: {filefound[0]}<br>")
+                f.write(
+                    "<style> table, td {border: 1px solid black; border-collapse: collapse;}tr:nth-child(even) {background-color: #f2f2f2;} .table th { background: #888888; color: #ffffff}.table.sticky th{ position:sticky; top: 0; }</style>"
+                )
+                f.write("<br/>")
+                f.write("")
+                f.write(f'<table class="table sticky">')
+                f.write(
+                    f"<tr><th>Account Desc</th><th>Usermane</th><th>Timestamp</th><th>Description</th><th>Identifier</th><th>Bundle ID</th></tr>"
+                )
+                for row in all_rows:
+                    f.write(
+                        f"<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td><td>{row[4]}</td><td>{row[5]}</td></tr>"
+                    )
+                f.write(f"</table></body></html>")
+                logfunc(f"Account Data function completed")
+        else:
+            logfunc("No Account Data available")
+    except:
+        logfunc("Error in Account Data Section.")
+
+
+def confaccts(filefound):
+    logfunc(f"Config Accounts function executing")
+
+    try:
+        if os.path.isdir(reportfolderbase + "Accounts/"):
+            pass
+        else:
+            os.makedirs(reportfolderbase + "Accounts")
+    except:
+        logfunc("Error creating confaccts() report directory")
+
+    try:
+        with open(reportfolderbase + "Accounts/Config Accounts.html", "w") as f:
+            f.write("<html><body>")
+            f.write("<h2>Config Accounts Report</h2>")
+            f.write(f"Config Accounts located at {filefound[0]}<br>")
+            f.write(
+                "<style> table, td {border: 1px solid black; border-collapse: collapse;}tr:nth-child(even) {background-color: #f2f2f2;} .table th { background: #888888; color: #ffffff}.table.sticky th{ position:sticky; top: 0; }</style>"
+            )
+            f.write("<br/>")
+            f.write("")
+            f.write(f"<table>")
+            f.write(f"<tr><td>Key</td><td>Values</td></tr>")
+            with open(filefound[0], "rb") as fp:
+                pl = plistlib.load(fp)
+                for key, val in pl.items():
+                    f.write(f"<tr><td>{key}</td><td>{val}</td></tr>")
+            f.write(f"</table></body></html>")
+    except:
+        logfunc("Error in Config Accounts function.")
+    logfunc("Config Accounts function completed.")
+
