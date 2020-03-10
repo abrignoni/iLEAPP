70c59c1678d6a4f3c9e1d5e874a430d58451f3fc
Agam Dua <agam@comfylabs.io>
2020-03-10 15:46:32 -0400

    Improved file path joining for temp

--- settings.py
+++ settings.py
@@ -2,14 +2,16 @@
 This module intends to be the source of truth for all configuration settings.
 """
 import datetime
+import os
 
 
 nl = "\n"
 now = datetime.datetime.now()
 currenttime = str(now.strftime("%Y-%m-%d_%A_%H%M%S"))
-reportfolderbase = "./ILEAPP_Reports_" + currenttime + "/"
-base = "/ILEAPP_Reports_" + currenttime + "/"
-temp = reportfolderbase + "temp/"
 
-# aliases for new code to use
+reportfolderbase = "./ILEAPP_Reports_" + currenttime + "/"
+# alias for new code to use
 report_folder_base = reportfolderbase
+
+base = "/ILEAPP_Reports_" + currenttime + "/"
+temp = os.path.join(report_folder_base, "temp", "")
