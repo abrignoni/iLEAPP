080ecfc2a2e21ee027fcfcb6bd519a99f907cc12
Agam Dua <agam@comfylabs.io>
2020-03-22 20:27:00 -0700

    Refactored util functions out of aggdict as discussed in #35

--- contrib/aggregated_dictionary/main.py
+++ contrib/aggregated_dictionary/main.py
@@ -5,7 +5,13 @@ import textwrap
 from jinja2 import Environment, FileSystemLoader
 
 from common import logfunc
-from contrib.utils import get_sql_output, silence_and_log
+from contrib.utils import (
+    DEFAULT_REPORT_TABLE_COLUMNS,
+    fetch_and_write_data,
+    get_sql_output,
+    silence_and_log,
+    write_html_template,
+)
 from settings import *
 
 from .sql import *
@@ -17,40 +23,6 @@ template_env = Environment(
 
 
 AGGREGATED_DICT_DIR_NAME = "Aggregated Dict/"
-DEFAULT_REPORT_TABLE_COLUMNS = ("Timestamp", "Key", "Value")
-
-
-@silence_and_log("Error in {category} section")
-def write_html_template(template, rows, db_path, category, report_table_columns):
-    template = template_env.get_template(template)
-    rendered = template.render(
-        rows=rows,
-        db_path=db_path,
-        category=category,
-        report_table_columns=report_table_columns,
-    )
-    fpath = f"{report_folder_base}{AGGREGATED_DICT_DIR_NAME}{category}.html"
-
-    with open(fpath, "w") as fp:
-        fp.write(rendered)
-
-
-@silence_and_log("Error in {category} section")
-def fetch_and_write_data(
-    query, db_path, category, report_table_columns=DEFAULT_REPORT_TABLE_COLUMNS
-):
-    logfunc(f"Aggregated dictionary {category} function executing")
-
-    rows = get_sql_output(query, db_path)
-    if not rows:
-        logfunc(f"No Aggregated dictionary {category} data available")
-        return
-
-    write_html_template(
-        "passcode_type.html", rows, db_path, category, report_table_columns
-    )
-
-    logfunc(f"Aggregated dictionary {category} function completed")
 
 
 def _create_aggregate_dict_dir():
@@ -66,21 +38,37 @@ def aggdict(filefound):
     fetch_and_write_data(
         passcode_type_query,
         filefound[0],
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
         category="Passcode Type",
         report_table_columns=list(DEFAULT_REPORT_TABLE_COLUMNS) + ["Passcode Type"],
     )
     fetch_and_write_data(
-        passcode_success_fail_query, filefound[0], category="Passcode Success-Fail"
+        passcode_success_fail_query,
+        filefound[0],
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
+        category="Passcode Success-Fail",
     )
     fetch_and_write_data(
         passcode_finger_template_query,
         filefound[0],
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
         category="Passcode Finger Template",
     )
-    fetch_and_write_data(scalars_query, filefound[0], category="Scalars")
+    fetch_and_write_data(
+        scalars_query,
+        filefound[0],
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
+        category="Scalars",
+    )
     fetch_and_write_data(
         distribution_keys_query,
         filefound[0],
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
         category="Distribution Keys",
         report_table_columns=["Day", "Seconds in a Day", "Key", "Value", "Table ID"],
     )
@@ -99,7 +87,13 @@ def dbbuff(filefound):
 
     report_table_columns = ["Value"] * 4
     write_html_template(
-        "passcode_type.html", rows, db_path, category, report_table_columns
+        "passcode_type.html",
+        rows,
+        db_path,
+        report_table_columns,
+        template_env,
+        AGGREGATED_DICT_DIR_NAME,
+        category=category,
     )
 
     logfunc(f"Aggregated dictionary DBbuffer function completed")
