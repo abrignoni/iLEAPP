15d761a199c1ba702d9899d28138a33146565d6d
Brigs <abrignoni@gmail.com>
2020-04-30 15:33:21 -0400

    New iLEAPP version

---
+++ ileapp.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+import scripts.report as report
+import shutil
+
+from scripts.search_files import *
+from scripts.ilapfuncs import *
+from scripts.ilap_artifacts import *
+from scripts.version_info import aleapp_version
+from time import process_time, gmtime, strftime
+
+def main():
+    parser = argparse.ArgumentParser(description='iLEAPP: iOS Logs, Events, and Plists Parser.')
+    parser.add_argument('-t', choices=['fs','tar','zip'], required=True, action="store", help="Input type (fs = extracted to file system folder)")
+    parser.add_argument('-o', '--output_path', required=True, action="store", help='Output folder path')
+    parser.add_argument('-i', '--input_path', required=True, action="store", help='Path to input file/folder')
+        
+    args = parser.parse_args()
+
+    input_path = args.input_path
+    output_path = os.path.abspath(args.output_path)
+    extracttype = args.t
+
+    if len(output_path) == 0:
+        print('No OUTPUT folder selected. Run the program again.')
+        return
+
+    if len(input_path) == 0:
+        print('No INPUT file or folder selected. Run the program again.')
+        return
+
+    if not os.path.exists(input_path):
+        print('INPUT file/folder does not exist! Run the program again.')
+        return  
+
+    out_params = OutputParameters(output_path)
+
+    crunch_artifacts(extracttype, input_path, out_params)
+        
+def crunch_artifacts(extracttype, input_path, out_params):
+    start = process_time()
+
+    logfunc('Procesing started. Please wait. This may take a few minutes...')
+
+    logfunc('\n--------------------------------------------------------------------------------------')
+    logfunc(f'iLEAPP v{aleapp_version}: iOS Logs, Events, and Plists Parser')
+    logfunc('Objective: Triage iOS Full System Extractions.')
+    logfunc('By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com')
+    logfunc('By: Yogesh Khatri | @SwiftForensics | swiftforensics.com')
+    logdevinfo()
+    
+    seeker = None
+    if extracttype == 'fs':
+        seeker = FileSeekerDir(input_path)
+
+    elif extracttype == 'tar':
+        seeker = FileSeekerTar(input_path, out_params.temp_folder)
+
+    elif extracttype == 'zip':
+        seeker = FileSeekerZip(input_path, out_params.temp_folder)
+
+    else:
+        logfunc('Error on argument -o (input type)')
+        return
+
+    # Now ready to run
+    logfunc(f'Artifact categories to parse: {str(len(tosearch))}')
+    logfunc(f'File/Directory selected: {input_path}')
+    logfunc('\n--------------------------------------------------------------------------------------')
+
+    log = open(os.path.join(out_params.report_folder_base, 'Script Logs', 'ProcessedFilesLog.html'), 'w+', encoding='utf8')
+    nl = '\n' #literal in order to have new lines in fstrings that create text files
+    log.write(f'Extraction/Path selected: {input_path}<br><br>')
+    
+    # Search for the files per the arguments
+    for key, val in tosearch.items():
+        artifact_pretty_name = val[0]
+        artifact_search_regex = val[1]
+        filefound = seeker.search(artifact_search_regex)
+        if not filefound:
+            logfunc()
+            logfunc(f'No files found for {key} -> {artifact_search_regex}')
+            log.write(f'No files found for {key} -> {artifact_search_regex}<br><br>')
+        else:
+            logfunc()
+            process_artifact(filefound, key, artifact_pretty_name, seeker, out_params.report_folder_base)
+            for pathh in filefound:
+                log.write(f'Files for {artifact_search_regex} located at {pathh}<br><br>')
+    log.close()
+
+    logfunc('')
+    logfunc('Processes completed.')
+    end = process_time()
+    run_time_secs =  end - start
+    run_time_HMS = strftime('%H:%M:%S', gmtime(run_time_secs))
+    logfunc("Processing time = {}".format(run_time_HMS))
+
+    logfunc('')
+    logfunc('Report generation started.')
+    report.generate_report(out_params.report_folder_base, run_time_secs, run_time_HMS, extracttype, input_path)
+    logfunc('Report generation Completed.')
+    logfunc('')
+    logfunc(f'Report location: {out_params.report_folder_base}')
+
+if __name__ == '__main__':
+    main()
---
+++ ileappGUI.py
@@ -0,0 +1,93 @@
+import ileapp
+import os
+import PySimpleGUI as sg
+import sys
+import webbrowser
+
+from scripts.ilapfuncs import *
+from scripts.version_info import aleapp_version
+from time import process_time, gmtime, strftime
+
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+
+normal_font = ("Helvetica", 12)
+
+layout = [  [sg.Text('iOS Logs, Events, And Protobuf Parser', font=("Helvetica", 22))],
+            [sg.Text('https://github.com/abrignoni/iLEAPP', font=("Helvetica", 14))],
+            [sg.Text('Select the file type or directory of the target Android full file system extraction for parsing.', font=normal_font)],
+            [sg.Radio('.Tar', "rad1", default=True, font=normal_font), sg.Radio('Directory', "rad1", font=normal_font), sg.Radio('.Zip', "rad1", font=normal_font)],
+            [sg.Text('Input File/Folder', size=(12, 1), font=normal_font), sg.Input(), sg.FileBrowse(font=normal_font)],
+            [sg.Text('Output Folder', size=(12, 1), font=normal_font), sg.Input(), sg.FolderBrowse(font=normal_font)],
+            [sg.Checkbox('Generate CSV output (Additional processing time)', size=(50, 1), default=False, font=normal_font)],
+            [sg.Output(size=(104,20))], #changed size from (88,20)
+            [sg.Submit('Process',font=normal_font), sg.Button('Close', font=normal_font)] ]
+            
+# Create the Window
+window = sg.Window(f'iLEAPP version {aleapp_version}', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (None, 'Close'):   # if user closes window or clicks cancel
+        break
+        
+    output_folder = values[4]
+    input_path = values[3]
+
+    if len(output_folder) == 0:
+        sg.PopupError('No OUTPUT folder selected. Run the program again.')
+        sys.exit()
+
+    if len(input_path) == 0:
+        sg.PopupError('No INPUT file or folder selected. Run the program again.')
+        sys.exit()
+
+    if not os.path.exists(input_path):
+        sg.PopupError('INPUT file/folder does not exist! Run the program again.')
+        sys.exit()
+
+    output_folder = os.path.abspath(output_folder)
+    
+    if event == 'Process':
+        if values[0] == True:
+            extracttype = 'tar'
+            if not input_path.lower().endswith('.tar'):
+                sg.PopupError('Input file does not have .tar extension! Run the program again.', input_path)
+                sys.exit()   
+                
+        elif values[1] == True:
+            extracttype = 'fs'
+            if not os.path.isdir(input_path):
+                sg.PopupError('Input path is not a valid folder. Run the program again.', input_path)
+                sys.exit()
+        
+        elif values[2] == True:
+            extracttype = 'zip'
+            if not input_path.lower().endswith('.zip'):
+                sg.PopupError('No file or no .zip extension selected. Run the program again.', input_path)
+                sys.exit()
+                
+        GuiWindow.window_handle = window
+        out_params = OutputParameters(output_folder)
+        ileapp.crunch_artifacts(extracttype, input_path, out_params)
+        
+        if values[5] == True:
+            start = process_time()
+            logfunc('')
+            logfunc(f'CSV export starting. This might take a while...')
+            html2csv(out_params.report_folder_base) 
+            end = process_time()
+            csv_time_secs =  end - start
+            csv_time_HMS = strftime('%H:%M:%S', gmtime(csv_time_secs))
+            logfunc("CSV processing time = {}".format(csv_time_HMS))
+
+        report_path = os.path.join(out_params.report_folder_base, 'index.html')
+        locationmessage = 'Report name: ' + report_path
+        sg.Popup('Processing completed', locationmessage)
+        
+        #basep = os.getcwd()
+        if report_path.startswith('\\\\'): # UNC path
+            report_path = report_path[2:]
+        webbrowser.open_new_tab('file://' + report_path)
+        break
+window.close()
