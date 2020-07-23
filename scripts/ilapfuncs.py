aba556da83386b6971f15087e2c4ac433d2a7aaf
ydkhatri <yogesh@swiftforensics.com>
2020-07-23 15:04:38 -0400

    added few functions from aleapp

--- scripts/ilapfuncs.py
+++ scripts/ilapfuncs.py
@@ -30,6 +30,43 @@ def is_platform_windows():
     '''Returns True if running on Windows'''
     return os.name == 'nt'
 
+def get_next_unused_name(path):
+    '''Checks if path exists, if it does, finds an unused name by appending -xx
+       where xx=00-99. Return value is new path.
+       If it is a file like abc.txt, then abc-01.txt will be the next
+    '''
+    folder, basename = os.path.split(path)
+    ext = None
+    if basename.find('.') > 0:
+        basename, ext = os.path.splitext(basename)
+    num = 1
+    new_name = basename
+    if ext != None:
+        new_name += f"{ext}"
+    while os.path.exists(os.path.join(folder, new_name)):
+        new_name = basename + "-{:02}".format(num)
+        if ext != None:
+            new_name += f"{ext}"
+        num += 1
+    return os.path.join(folder, new_name)
+
+def does_column_exist_in_db(db, table_name, col_name):
+    '''Checks if a specific col exists'''
+    col_name = col_name.lower()
+    try:
+        db.row_factory = sqlite3.Row # For fetching columns by name
+        query = f"pragma table_info('{table_name}');"
+        cursor = db.cursor()
+        cursor.execute(query)
+        all_rows = cursor.fetchall()
+        for row in all_rows:
+            if row['name'].lower() == col_name:
+                return True
+    except sqlite3.Error as ex:
+        print(f"Query error, query={query} Error={str(ex)}")
+        pass
+    return False
+
 class GuiWindow:
     '''This only exists to hold window handle if script is run from GUI'''
     window_handle = None # static variable 
