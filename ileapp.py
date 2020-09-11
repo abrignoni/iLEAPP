831e834e875a5898dee8ed57703d14091ce414da
Brigs <abrignoni@gmail.com>
2020-09-11 11:04:42 -0400

    Option -p
    Option that exports a list of the artifact paths located in the tosearch dictionary to a text file named path_list.txt.

--- ileapp.py
+++ ileapp.py
@@ -11,37 +11,68 @@ from time import process_time, gmtime, strftime
 
 def main():
     parser = argparse.ArgumentParser(description='iLEAPP: iOS Logs, Events, and Plists Parser.')
-    parser.add_argument('-t', choices=['fs','tar','zip', 'gz'], required=True, action="store", help="Input type (fs = extracted to file system folder)")
-    parser.add_argument('-o', '--output_path', required=True, action="store", help='Output folder path')
-    parser.add_argument('-i', '--input_path', required=True, action="store", help='Path to input file/folder')
+    parser.add_argument('-t', choices=['fs','tar','zip', 'gz'], required=False, action="store", help="Input type (fs = extracted to file system folder)")
+    parser.add_argument('-o', '--output_path', required=False, action="store", help='Output folder path')
+    parser.add_argument('-i', '--input_path', required=False, action="store", help='Path to input file/folder')
+    parser.add_argument('-p', '--artifact_paths', required=False, action="store_true", help='Text file list of artifact paths')
         
     args = parser.parse_args()
-
-    input_path = args.input_path
-    output_path = os.path.abspath(args.output_path)
-    extracttype = args.t
-
-    if len(output_path) == 0:
-        print('No OUTPUT folder selected. Run the program again.')
+    
+    if args.artifact_paths == True:
+        print('Artifact path list generation started.')
+        print('')
+        with open('path_list.txt', 'a') as paths:
+            for key, value in tosearch.items():
+                if type(value[1]) is tuple:
+                    for x in value[1]:
+                        paths.write(x+'\n')
+                        print(x)
+                else:
+                    paths.write(value[1]+'\n')
+                    print(value[1])
+        print('')
+        print('Artifact path list generation completed')    
         return
+
+    else:
+        input_path = args.input_path
+        extracttype = args.t
         
-    if len(input_path) == 0:
-        print('No INPUT file or folder selected. Run the program again.')
-        return
+        if args.output_path == None:
+            parser.error('No OUTPUT folder path provided')
+            return
+        else:
+            output_path = os.path.abspath(args.output_path)
+        
+        if output_path == None:
+            parser.error('No OUTPUT folder selected. Run the program again.')
+            return
+            
+        if input_path == None:
+            parser.error('No INPUT file or folder selected. Run the program again.')
+            return
+        
+        if args.t == None:
+            parser.error('No INPUT file or folder selected. Run the program again.')
+            return
 
-    if not os.path.exists(input_path):
-        print('INPUT file/folder does not exist! Run the program again.')
-        return  
+        if not os.path.exists(input_path):
+            parser.error('INPUT file/folder does not exist! Run the program again.')
+            return
+        
+        if not os.path.exists(output_path):
+            parser.error('OUTPUT folder does not exist! Run the program again.')
+            return  
 
-    # ios file system extractions contain paths > 260 char, which causes problems
-    # This fixes the problem by prefixing \\?\ on each windows path.
-    if is_platform_windows():
-        if input_path[1] == ':' and extracttype =='fs': input_path = '\\\\?\\' + input_path.replace('/', '\\')
-        if output_path[1] == ':': output_path = '\\\\?\\' + output_path.replace('/', '\\')
+        # ios file system extractions contain paths > 260 char, which causes problems
+        # This fixes the problem by prefixing \\?\ on each windows path.
+        if is_platform_windows():
+            if input_path[1] == ':' and extracttype =='fs': input_path = '\\\\?\\' + input_path.replace('/', '\\')
+            if output_path[1] == ':': output_path = '\\\\?\\' + output_path.replace('/', '\\')
 
-    out_params = OutputParameters(output_path)
+        out_params = OutputParameters(output_path)
 
-    crunch_artifacts(tosearch, extracttype, input_path, out_params, 1)
+        crunch_artifacts(tosearch, extracttype, input_path, out_params, 1)
 
 def crunch_artifacts(search_list, extracttype, input_path, out_params, ratio):
     start = process_time()
