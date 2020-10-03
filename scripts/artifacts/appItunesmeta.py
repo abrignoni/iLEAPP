921d1d99ba41a87bb5dded6787be4df01bdbbe9f
Brigs <abrignoni@gmail.com>
2020-10-03 18:22:41 -0400

    Metadata from installed apps
    Combiation of data in the iTunesmetadata and bundlemetadata plists in one report.

---
+++ scripts/artifacts/appItunesmeta.py
@@ -0,0 +1,63 @@
+import biplist
+import pathlib
+import os
+import nska_deserialize as nd
+
+from scripts.artifact_report import ArtifactHtmlReport
+from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows
+
+
+def get_appItunesmeta(files_found, report_folder, seeker):
+    data_list = []       
+    for file_found in files_found:
+        file_found = str(file_found)
+
+        if file_found.endswith('iTunesMetadata.plist'):
+            plist = biplist.readPlist(file_found)
+            
+            purchasedate = (plist['com.apple.iTunesStore.downloadInfo']['purchaseDate'])
+            bundleid = (plist['softwareVersionBundleId'])
+            itemname = (plist['itemName'])
+            artistname = (plist['artistName'])
+            versionnum = (plist['bundleShortVersionString'])
+            downloadedby = (plist['com.apple.iTunesStore.downloadInfo']['accountInfo']['AppleID'])
+            genre = (plist['genre'])
+            factoryinstall = (plist['isFactoryInstall'])
+            appreleasedate = (plist['releaseDate'])
+            sourceapp = (plist['sourceApp'])
+            sideloaded = (plist['sideLoadedDeviceBasedVPP'])
+            variantid = (plist['variantID'])
+            
+            p = pathlib.Path(file_found)
+            parent = p.parent
+
+            itunes_metadata_path = (os.path.join(parent, "BundleMetadata.plist"))
+            if os.path.exists(itunes_metadata_path):
+                with open(itunes_metadata_path, 'rb') as f:
+                    deserialized_plist = nd.deserialize_plist(f)
+                    install_date = (deserialized_plist['installDate'])
+            else:
+                install_date = ''
+    
+            
+            data_list.append((install_date, purchasedate, bundleid, itemname, artistname, versionnum, downloadedby, genre, factoryinstall, appreleasedate, sourceapp, sideloaded, variantid, parent))   
+
+    if len(data_list) > 0:
+        fileloc = 'See source file location column'
+        description = 'iTunes & Bundle ID Metadata contents for apps'
+        report = ArtifactHtmlReport('Apps - Itunes & Bundle Metadata')
+        report.start_artifact_report(report_folder, 'Apps - Itunes Metadata', description)
+        report.add_script()
+        data_headers = ('Installed Date', 'App Purchase Date','Bundle ID', 'Item Name', 'Artist Name', 'Version Number', 'Downloaded by', 'Genre', 'Factory Install', 'App Release Date', 'Source App', 'Sideloaded?', 'Variant ID', 'Source File Location')     
+        report.write_artifact_data_table(data_headers, data_list, fileloc)
+        report.end_artifact_report()
+        
+        tsvname = 'Apps - Itunes Bundle Metadata'
+        tsv(report_folder, data_headers, data_list, tsvname)
+        
+        tlactivity = 'Apps - Itunes Bundle Metadata'
+        timeline(report_folder, tlactivity, data_list, data_headers)
+    else:
+        logfunc('No data on Apps - Itunes Bundle Metadata')
+
+    
