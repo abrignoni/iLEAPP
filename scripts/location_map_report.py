e8a0a9a3d49e93f123268382ac1ce30d34adbd67
XperyLab <matthieu@xperybab.fr>
2020-11-10 11:31:40 +0100

    driverless location map - no geopandas

--- scripts/location_map_report.py
+++ scripts/location_map_report.py
@@ -1,18 +1,15 @@
 # coding: utf-8
 #Import the necessary Python modules
 import pandas as pd
-import geopandas as gpd
 import folium
-from folium import IFrame
 from folium.plugins import TimestampedGeoJson
-import shapely
 from shapely.geometry import Point
 import os
-import fiona
 from datetime import datetime
 from branca.element import Template, MacroElement
 import html
 from scripts.location_map_constants import iLEAPP_KMLs, defaultShadowUrl, defaultIconUrl, colors, legend_tag, legend_title_tag, legend_div, template_part1, template_part2
+import sqlite3 
 
 from scripts.artifact_report import ArtifactHtmlReport
 
@@ -25,7 +22,7 @@ def geodfToFeatures(df, f, props):
     times = []
     for i,row in df[df.Description.str.contains(f)].iterrows():
         coords.append(
-            [row.geometry.x,row.geometry.y]
+            [row.Point.x,row.Point.y]
         )
         times.append(datetime.strptime(row.Name,'%Y-%m-%d %H:%M:%S').isoformat())
     return {  
@@ -57,23 +54,15 @@ def generate_location_map(reportfolderbase,legend_title):
         return
 
     location_path = os.path.join(reportfolderbase, 'LOCATIONS')
-    gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
-
-    #import KML files to GeoPandas
-    #start with empty df
-    df = gpd.GeoDataFrame()
-
-    for filename in os.listdir(KML_path):
-        if filename.endswith("kml")  and not 'Photo' in filename:
-            s = gpd.read_file(os.path.join(KML_path,filename), driver='KML')
-            df = df.append(s, ignore_index=True)
+    db = sqlite3.connect(os.path.join(KML_path,"_latlong.db"))
+    df = pd.read_sql_query("SELECT key as Name, Activity as Description, latitude, longitude FROM data ;", db)
+    df["Point"] = df.apply(lambda row: Point(float(row['longitude']),float(row['latitude']),.0), axis=1)
 
     #sorting is needed for correct display
     df.sort_values(by=['Name'],inplace=True)
 
-    #Parse KML data and add to Folium Map
-    data_names = df.Description.str.extract(r'.{30} - (.*)')[0].unique()
-
+    #Parse geo data and add to Folium Map
+    data_names = df[~df.Description.str.contains('Photos')].Description.unique()
     featuresProp = {}
 
     for c,d in zip(colors, data_names):
@@ -99,10 +88,15 @@ def generate_location_map(reportfolderbase,legend_title):
                 'shadowUrl': defaultShadowUrl,
             }
 
-    location_map = folium.Map([df.iloc[0].geometry.y,df.iloc[0].geometry.x], prefer_canvas=True, zoom_start = 6)
+    location_map = folium.Map([df.iloc[0].Point.y,df.iloc[0].Point.x], prefer_canvas=True, zoom_start = 6)
+    bounds = (df[~df.Description.str.contains('Photos')]['longitude'].min(),
+              df[~df.Description.str.contains('Photos')]['latitude'].min(),
+              df[~df.Description.str.contains('Photos')]['longitude'].max(),
+              df[~df.Description.str.contains('Photos')]['latitude'].max(),
+             )
     location_map.fit_bounds([
-            (df.geometry.total_bounds[1],df.geometry.total_bounds[0]),
-            (df.geometry.total_bounds[3],df.geometry.total_bounds[2]),
+            (bounds[1],bounds[0]),
+            (bounds[3],bounds[2]),
         ]
     )
 
