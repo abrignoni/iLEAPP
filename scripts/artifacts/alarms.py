81db6c84ba1503e43a4ca830eb03244c99db2940
any333 <anna-mariya.mateyna@gmx.de>
2021-01-18 22:57:16 +0100

    Fixed a bug and optimized a bit

--- scripts/artifacts/alarms.py
+++ scripts/artifacts/alarms.py
@@ -11,28 +11,31 @@ def get_alarms(files_found, report_folder, seeker):
         with open(file_found, "rb") as plist_file:
             pl = plistlib.load(plist_file)
 
-            for alarms in pl['MTAlarms']['MTAlarms']:
-                alarms_dict = alarms['$MTAlarm']
+            if 'MTAlarms' in pl:
+                if 'MTAlarms' in pl['MTAlarms']:
+                    for alarms in pl['MTAlarms']['MTAlarms']:
+                        alarms_dict = alarms['$MTAlarm']
 
-                alarm_title = check_if_key_exists('MTAlarmTitle', alarms_dict, 'Alarm')
-                fire_date = check_if_key_exists('MTAlarmFireDate', alarms_dict)
-                dismiss_date = check_if_key_exists('MTAlarmDismissDate', alarms_dict)
+                        alarm_title = alarms_dict.get('MTAlarmTitle', 'Alarm')
+                        fire_date = alarms_dict.get('MTAlarmFireDate', '')
+                        dismiss_date = alarms_dict.get('MTAlarmDismissDate', '')
 
-                data_list.append((alarm_title, alarms_dict['MTAlarmEnabled'], fire_date, dismiss_date,
-                                  alarms_dict['MTAlarmLastModifiedDate'],
-                                  alarms_dict['MTAlarmRepeatSchedule'], alarms_dict['MTAlarmSound']['$MTSound']['MTSoundToneID'],
-                                  alarms_dict['MTAlarmIsSleep'], alarms_dict['MTAlarmBedtimeDoNotDisturb'], ''))
+                        data_list.append((alarm_title, alarms_dict['MTAlarmEnabled'], fire_date, dismiss_date,
+                                          alarms_dict['MTAlarmLastModifiedDate'], alarms_dict['MTAlarmRepeatSchedule'],
+                                          alarms_dict['MTAlarmSound']['$MTSound']['MTSoundToneID'],
+                                          alarms_dict['MTAlarmIsSleep'], alarms_dict['MTAlarmBedtimeDoNotDisturb'], ''))
 
-            for sleep_alarms in pl['MTAlarms']['MTSleepAlarm']:
-                sleep_alarm_dict = pl['MTAlarms']['MTSleepAlarm'][sleep_alarms]
+                if 'MTSleepAlarm' in pl['MTAlarms']:
+                    for sleep_alarms in pl['MTAlarms']['MTSleepAlarm']:
+                        sleep_alarm_dict = pl['MTAlarms']['MTSleepAlarm'][sleep_alarms]
 
-                alarm_title = check_if_key_exists('MTAlarmTitle', sleep_alarm_dict, 'Bedtime')
+                        alarm_title = sleep_alarm_dict.get('MTAlarmTitle', 'Bedtime')
 
-                data_list.append((alarm_title, sleep_alarm_dict['MTAlarmEnabled'], sleep_alarm_dict['MTAlarmFireDate'],
-                                  sleep_alarm_dict['MTAlarmDismissDate'], sleep_alarm_dict['MTAlarmLastModifiedDate'],
-                                  sleep_alarm_dict['MTAlarmRepeatSchedule'], sleep_alarm_dict['MTAlarmSound']['$MTSound']['MTSoundToneID'],
-                                  sleep_alarm_dict['MTAlarmIsSleep'], sleep_alarm_dict['MTAlarmBedtimeDoNotDisturb'],
-                                  sleep_alarm_dict['MTAlarmBedtimeFireDate']))
+                        data_list.append((alarm_title, sleep_alarm_dict['MTAlarmEnabled'], sleep_alarm_dict['MTAlarmFireDate'],
+                                          sleep_alarm_dict['MTAlarmDismissDate'], sleep_alarm_dict['MTAlarmLastModifiedDate'],
+                                          sleep_alarm_dict['MTAlarmRepeatSchedule'], sleep_alarm_dict['MTAlarmSound']['$MTSound']['MTSoundToneID'],
+                                          sleep_alarm_dict['MTAlarmIsSleep'], sleep_alarm_dict['MTAlarmBedtimeDoNotDisturb'],
+                                          sleep_alarm_dict['MTAlarmBedtimeFireDate']))
 
     if len(data_list) > 0:
         report = ArtifactHtmlReport('Alarms')
@@ -50,12 +53,3 @@ def get_alarms(files_found, report_folder, seeker):
 
     else:
         logfunc('No Alarms found')
-
-
-def check_if_key_exists(dict_key, alarms_dict, replace_value=''):
-    if dict_key in alarms_dict:
-        return alarms_dict[dict_key]
-    else:
-        if not replace_value:
-            return ''
-        return replace_value
