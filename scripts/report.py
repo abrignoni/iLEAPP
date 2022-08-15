7bbc33d1eaadd6895a359a03f689364e0d0e9ed4
abrignoni <abrignoni@gmail.com>
2022-08-14 23:05:13 -0400

    Appintents
    Research referenced:
    https://bluecrewforensics.com/2022/03/07/ios-app-intents/

--- scripts/report.py
+++ scripts/report.py
@@ -31,6 +31,8 @@ def get_icon_name(category, artifact):
         icon = 'book-open'
     elif category == 'AIRTAGS':
         icon = 'map-pin'
+    elif category == 'INTENTS':
+        icon = 'command'
     elif category == 'ALARMS':
         icon = 'clock'
     elif category == 'ALLTRAILS':
