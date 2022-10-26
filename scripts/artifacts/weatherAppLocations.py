ec9c23f4dfa7a9e0eab66dcb0e23d965ad16c345
abrignoni <abrignoni@gmail.com>
2022-10-25 21:06:10 -0400

    Updates to appintents
    Update to signal and whatsapp intents.
    Fix weather app locations.

--- scripts/artifacts/weatherAppLocations.py
+++ scripts/artifacts/weatherAppLocations.py
@@ -1,5 +1,6 @@
 import plistlib
 import time
+import datetime
 
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, kmlgen, timeline, is_platform_windows
@@ -7,30 +8,48 @@ from scripts.ilapfuncs import logfunc, tsv, kmlgen, timeline, is_platform_window
 
 def get_weatherAppLocations(files_found, report_folder, seeker, wrap_text):
     data_list = []
-
+    data_list_two_one = []
     for file_found in files_found:
         file_found = str(file_found)
         with open(file_found, "rb") as plist_file:
             plist_content = plistlib.load(plist_file)
             
-            if plist_content.get('Cities', '0') == '0':
-              logfunc('No cities available')
-              return
-            
-            for city in plist_content['Cities']:
-                update_time = city['UpateTime']
-                update_time_formatted = update_time.strftime('%Y-%m-%d %H:%M:%S')
+          
+            if plist_content['PrefsVersion'] == '2.1':
+              lastupdated = (plist_content['LastUpdated'])
+              
+              if plist_content.get('Cities', '0') == '0':
+                logfunc('No cities available')
+                return
 
-                data_list.append((update_time_formatted, 'Added from User', '', city['Lat'],
-                                  city['Lon'], city['Name'], city['Country'], city['SecondsFromGMT']))
-
-            local_weather = plist_content['LocalWeather']
-            local_update_time = local_weather['UpateTime']
-            local_update_time_formatted = local_update_time.strftime('%Y-%m-%d %H:%M:%S')
-            last_location_update = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(plist_content.get('LastLocationUpdateTime')))
-
-            data_list.append((local_update_time_formatted, 'Local', last_location_update, local_weather['Lat'],
-                              local_weather['Lon'], local_weather['Name'], local_weather['Country'], local_weather['SecondsFromGMT']))
+              for x in plist_content['Cities']:
+                lon = x.get('Lon','')
+                lat = x.get('Lat','')
+                name = x.get('Name','')
+                country = x.get('Country','')
+                timezone = x.get('TimeZone','')
+                cityupdate = x.get('CityTimeZoneUpdateDateKey','')
+                
+                data_list_two_one.append((lastupdated, name, country, timezone, cityupdate, lat, lon))
+            else:
+              if plist_content.get('Cities', '0') == '0':
+                logfunc('No cities available')
+                return
+              
+              for city in plist_content['Cities']:
+                  update_time = city.get('UpateTime','')
+                  update_time_formatted = update_time.strftime('%Y-%m-%d %H:%M:%S')
+  
+                  data_list.append((update_time_formatted, 'Added from User', '', city['Lat'],
+                                    city['Lon'], city['Name'], city['Country'], city['SecondsFromGMT']))
+  
+              local_weather = plist_content['LocalWeather']
+              local_update_time = local_weather['UpateTime']
+              local_update_time_formatted = local_update_time.strftime('%Y-%m-%d %H:%M:%S')
+              last_location_update = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(plist_content.get('LastLocationUpdateTime')))
+  
+              data_list.append((local_update_time_formatted, 'Local', last_location_update, local_weather['Lat'],
+                                local_weather['Lon'], local_weather['Name'], local_weather['Country'], local_weather['SecondsFromGMT']))
 
     if len(data_list) > 0:
         report = ArtifactHtmlReport('Weather App Locations')
@@ -49,6 +68,22 @@ def get_weatherAppLocations(files_found, report_folder, seeker, wrap_text):
         kmlactivity = 'Weather App Locations'
         kmlgen(report_folder, kmlactivity, data_list, data_headers)
 
+    elif len(data_list_two_one) > 0:
+        report = ArtifactHtmlReport('Weather App Locations')
+        report.start_artifact_report(report_folder, 'Weather App Locations')
+        report.add_script()
+        data_headers = ("Update Time", "Name", "Country", "TimeZone", "City Timezone Update Key","Latitude", "Longitude")
+        report.write_artifact_data_table(data_headers, data_list_two_one, file_found)
+        report.end_artifact_report()
+      
+        tsvname = 'Weather App Locations'
+        tsv(report_folder, data_headers, data_list_two_one, tsvname)
+      
+        tlactivity = 'Weather App Locations'
+        timeline(report_folder, tlactivity, data_list_two_one, data_headers)
+      
+        kmlactivity = 'Weather App Locations'
+        kmlgen(report_folder, kmlactivity, data_list_two_one, data_headers)
     else:
         logfunc('No data available for Weather App Locations')
 
