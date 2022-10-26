e47786d5dae958e70bfd3be54edd858c7216cd05
abrignoni <abrignoni@gmail.com>
2022-10-26 00:24:12 -0400

    Update appleMapsSearchHistory.py
    Added full decoded protobuf data to report.
    Your eyes will bleed but no data will be missed.

--- scripts/artifacts/appleMapsSearchHistory.py
+++ scripts/artifacts/appleMapsSearchHistory.py
@@ -1,6 +1,7 @@
 import plistlib
 import blackboxprotobuf
 import base64
+import pprint
 
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows
@@ -41,7 +42,7 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                 if isinstance(d, dict): #records
                     for e, f in d.items():
                         guid = e
-                        shortadd = pname = app = location = geo1 = geo2 = geo3 = geo4 = geo5 = geo6 = usersearch1 = usersearch2 = shortlat = shortlon = usersearchnotinproto = ''
+                        shortadd = pname = app = location = geo1 = geo2 = geo3 = geo4 = geo5 = geo6 = usersearch1 = usersearch2 = shortlat = shortlon = usersearchnotinproto = items = currentlocation = ''
                         for g, h in f.items():
                             
                             if g == 'modificationDate':
@@ -50,57 +51,68 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                 
                                 protostuff, types = blackboxprotobuf.decode_message(h)
                                 #pp.pprint(protostuff)
-                                #print(protostuff)
+                                items = (protostuff)
                                 if protostuff.get('7'):
-                                    for look in (protostuff.get('7')['1']['1'][0]['2']['1']['4']):
-                                        if look.get('8'):
-                                            data = (look['8'].get('1'))
-                                            if data is not None:
-                                                pname = (data['10']['3'].decode())
+                                    try:
+                                        for look in (protostuff.get('7')['1']['1'][0]['2']['1']['4']):
+                                            if look.get('8'):
+                                                data = (look['8'].get('1'))
+                                                if data is not None:
+                                                    pname = (data['10']['3'].decode())
+                                    except:
+                                        #yelp like stuff
+                                        pp = pprint.PrettyPrinter(indent = 0)
+                                        items = pp.pformat(protostuff.get('7')['1']['1'][1])
+                                        
                                                 
                                 if protostuff.get('6'):
-                                    location = protostuff['6']['1'].decode()
-                                    location = location + ', ' + protostuff['6']['2'].decode() #GPs after it?
-                                    
+                                    try:
+                                        location = protostuff['6']['1'].decode()
+                                        location = location + ', ' + protostuff['6']['2'].decode() #GPs after it?
+                                    except:
+                                        pass
                                 try:    
                                     if protostuff.get('8'):
                                         if (protostuff['8']['2']['1'].get('201')):
                                             usersearchnotinproto = (protostuff['8']['2']['1']['201']['2']['2']['1'].decode())
                                 except:
                                     pass
+                                    try:
+                                        for mira in (protostuff['8']['2']['1']['4']):
+                                            #print(mira)
+                                            if mira.get('11'):
+                                                if mira['11']['2'].startswith(b'pl'):
+                                                    try:
+                                                        longstuff = longbase64proto(mira['11']['2'].decode(), longtypes)
+                                                        app = longstuff['1']['1'].decode()
+                                                        location = longstuff['8']['1']['3'].decode()
+                                                        geo1 = longstuff['8']['1']['4']['1']['5']
+                                                        geo2 = longstuff['8']['1']['4']['1']['6']
+                                                        geo3 = longstuff['8']['1']['4']['1']['7']
+                                                        geo4 = longstuff['8']['1']['4']['1']['8']
+                                                        usersearch1 = longstuff['8']['1']['7']['12']['1'].decode()
+                                                        usersearch2 = longstuff['8']['1']['7']['10000'].decode()
+                                                        geo5 = longstuff['8']['1']['7']['5']['1']['1']
+                                                        geo6 = longstuff['8']['1']['7']['5']['1']['2']
+                                                    except:
+                                                        pass
+                                                        
+                                                    try: 
+                                                        
+                                                        shortstuff = shortbase64proto(mira['11']['2'].decode(), shorttypes)
+                                                        
+                                                        shortlat = (shortstuff['8']['4']['4']['1']['1'])
+                                                        shortlon = (shortstuff['8']['4']['4']['1']['2'])
+                                                        
+                                                        
+                                                    except:
+                                                        pass
+                                                    
+                                                    if mira.get('8'):
+                                                        pname = (mira.get('8')['1']['10']['3'].decode())
+                                    except:
+                                        currentlocation = (protostuff['8']['2']['8']['3'].decode())
                                         
-                                    for mira in (protostuff['8']['2']['1']['4']):
-                                        #print(mira)
-                                        if mira.get('11'):
-                                            if mira['11']['2'].startswith(b'pl'):
-                                                try:
-                                                    longstuff = longbase64proto(mira['11']['2'].decode(), longtypes)
-                                                    app = longstuff['1']['1'].decode()
-                                                    location = longstuff['8']['1']['3'].decode()
-                                                    geo1 = longstuff['8']['1']['4']['1']['5']
-                                                    geo2 = longstuff['8']['1']['4']['1']['6']
-                                                    geo3 = longstuff['8']['1']['4']['1']['7']
-                                                    geo4 = longstuff['8']['1']['4']['1']['8']
-                                                    usersearch1 = longstuff['8']['1']['7']['12']['1'].decode()
-                                                    usersearch2 = longstuff['8']['1']['7']['10000'].decode()
-                                                    geo5 = longstuff['8']['1']['7']['5']['1']['1']
-                                                    geo6 = longstuff['8']['1']['7']['5']['1']['2']
-                                                except:
-                                                    pass
-                                                    
-                                                try: 
-                                                    
-                                                    shortstuff = shortbase64proto(mira['11']['2'].decode(), shorttypes)
-                                                    
-                                                    shortlat = (shortstuff['8']['4']['4']['1']['1'])
-                                                    shortlon = (shortstuff['8']['4']['4']['1']['2'])
-                                                    
-                                                    
-                                                except:
-                                                    pass
-                                                
-                                                if mira.get('8'):
-                                                    pname = (mira.get('8')['1']['10']['3'].decode())
                                 try: 
                                     if protostuff.get('7'):
                                         shortstuff = shortbase64proto(protostuff.get('7')['1']['1'][0]['2']['1']['4'][8]['11']['2'].decode(), shorttypes)
@@ -148,10 +160,11 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                                     except:
                                                         pass
                                     except:
-                                        pass
+                                        pp = pprint.PrettyPrinter(indent = 0)
+                                        items = pp.pformat(protostuff['7']['1']['1'][0]['2']['1']['4'])
                                         
-                                data_list.append((modificationdate,app,location,shortadd,pname,shortlat,shortlon,usersearchnotinproto,usersearch1, usersearch2,geo1,geo2,geo3,geo4,geo5,geo6,guid ))
-                                modificationdate = app = locatio = shortadd = pname = shortlat = shortlon = usersearch1 = usersearch2 = geo1 = geo2 = geo3 = geo4 = geo5 = geo6 = guid = '' 
+                                data_list.append((modificationdate,app,location,shortadd,pname,shortlat,shortlon,usersearchnotinproto,usersearch1, usersearch2,geo1,geo2,geo3,geo4,geo5,geo6,guid,currentlocation,items ))
+                                modificationdate = app = locatio = shortadd = pname = shortlat = shortlon = usersearch1 = usersearch2 = geo1 = geo2 = geo3 = geo4 = geo5 = geo6 = guid = items = currentlocation = '' 
         
         
 
@@ -159,7 +172,7 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
         report = ArtifactHtmlReport('Apple Maps Search History')
         report.start_artifact_report(report_folder, 'Apple Maps Search History')
         report.add_script()
-        data_headers = ('Timestamp','App','Location','Short Address','Place Name','Latitude','Longitude','Search Not in Protobuf','Search Term','Search Term','Lat1','Lon1','Lat2','Lon2','Lat2','Lon3','Record GUID' )
+        data_headers = ('Timestamp','App','Location','Short Address','Place Name','Latitude','Longitude','Search Not in Protobuf','Search Term','Search Term','Lat1','Lon1','Lat2','Lon2','Lat2','Lon3','Record GUID','Current Location','Items' )
         report.write_artifact_data_table(data_headers, data_list, file_found)
         report.end_artifact_report()
 
