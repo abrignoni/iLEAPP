067d3eb256e4a1ff0b6065e4e01a402106605971
theAtropos4n6 (Evangelos D.) <70748441+theAtropos4n6@users.noreply.github.com>
2023-03-27 23:56:45 +0300

    Update version_info.py

--- scripts/version_info.py
+++ scripts/version_info.py
@@ -35,4 +35,5 @@ aleapp_contributors = [
     ['Troy Schnack', '', '@TroySchnack', ''],
     ['Bo Amos', '', '@Bo_Knows_65', ''],
     ['Joshua James', 'https://dfir.science', '@dfirscience', 'https://github.com/dfirscience']
+    ['Evangelos Dragonas', 'https://atropos4n6.com/','@theAtropos4n6','https://github.com/theAtropos4n6']
 ]
