0d16a85e59da37a879319cb31ccb926c8408f0de
upintheairsheep2 <107060941+upintheairsheep2@users.noreply.github.com>
2023-04-30 20:02:19 -0700

    Create AshHistory.py
    For iSH

---
+++ scripts/artifacts/AshHistory.py
@@ -0,0 +1,33 @@
+import codecs
+import csv
+from scripts.artifact_report import ArtifactHtmlReport
+from scripts.ilapfuncs import logfunc, tsv, is_platform_windows
+def get_AshHistory(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    file_found = str(files_found[0])
+    counter = 1
+    with codecs.open(file_found, 'r', 'utf-8-sig') as csvfile:
+        for row in csvfile:
+            data_list.append((counter, row))
+            counter += 1
+            
+    if len(data_list) > 0:
+        report = ArtifactHtmlReport('Alphine Linux Bash History')
+        report.start_artifact_report(report_folder, f'Alphine Linux Bash History')
+        report.add_script()
+        data_headers = ('Order', 'Command')
+        report.write_artifact_data_table(data_headers, data_list, file_found)
+        report.end_artifact_report()
+        
+        tsvname = f'Alphine Linux Bash History'
+        tsv(report_folder, data_headers, data_list, tsvname)
+        
+    else:
+        logfunc(f'No Alphine Linux Bash History file available')
+    
+__artifacts__ = {
+        "Alphine Linux Bash History": (
+                "Alphine Linux Bash History",
+                ('*/.ash_history'),
+                get_AshHistory)
+}
