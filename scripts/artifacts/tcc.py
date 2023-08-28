50917b1a91607b580fe3fea66d79d9587deff55a
Alexis Bringoni <abrignoni@gmail.com>
2023-08-28 17:55:40 -0400

    Update tcc.py
    Eliminate iOS version constraint

--- scripts/artifacts/tcc.py
+++ scripts/artifacts/tcc.py
@@ -17,49 +17,44 @@ def get_tcc(files_found, report_folder, seeker, wrap_text):
             break
         
     db = open_sqlite_db_readonly(file_found)
-    iOSversion = scripts.artifacts.artGlobals.versionf
-    if version.parse(iOSversion) >= version.parse("9"):
-        cursor = db.cursor()
-        cursor.execute('''
-        select datetime(last_modified,'unixepoch'),
-        client,
-        service,
-        case auth_value
-            when 0 then 'Not allowed'
-            when 2 then 'Allowed'
-            when 3 then 'Limited'
-            else auth_value
-        end
-        from access
-        order by client
-        ''')
+    cursor = db.cursor()
+    cursor.execute('''
+    select datetime(last_modified,'unixepoch'),
+    client,
+    service,
+    case auth_value
+        when 0 then 'Not allowed'
+        when 2 then 'Allowed'
+        when 3 then 'Limited'
+        else auth_value
+    end
+    from access
+    order by client
+    ''')
+    
+    all_rows = cursor.fetchall()
+    usageentries = len(all_rows)
+    if usageentries > 0:
+        data_list =[]
+        for row in all_rows: 
+            data_list.append((row[0], row[1], row[2].replace("kTCCService",""), row[3]))
+
+    if usageentries > 0:
+        report = ArtifactHtmlReport('TCC - Permissions')
+        report.start_artifact_report(report_folder, 'TCC - Permissions')
+        report.add_script()
+        data_headers = ('Last Modified Timestamp','Bundle ID','Service','Access')
+        report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
+        report.end_artifact_report()
         
-        all_rows = cursor.fetchall()
-        usageentries = len(all_rows)
-        if usageentries > 0:
-            data_list =[]
-            for row in all_rows: 
-                data_list.append((row[0], row[1], row[2].replace("kTCCService",""), row[3]))
-
-        if usageentries > 0:
-            report = ArtifactHtmlReport('TCC - Permissions')
-            report.start_artifact_report(report_folder, 'TCC - Permissions')
-            report.add_script()
-            data_headers = ('Last Modified Timestamp','Bundle ID','Service','Access')
-            report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
-            report.end_artifact_report()
-            
-            tsvname = 'TCC - Permissions'
-            tsv(report_folder, data_headers, data_list, tsvname)
-            
-            tlactivity = 'TCC - Permissions'
-            timeline(report_folder, tlactivity, data_list, data_headers)
-            
-        else:
-            logfunc('No data available in TCC database.')
-
-        db.close()
-        return      
+        tsvname = 'TCC - Permissions'
+        tsv(report_folder, data_headers, data_list, tsvname)
+        
+        tlactivity = 'TCC - Permissions'
+        timeline(report_folder, tlactivity, data_list, data_headers)
+        
+    else:
+        logfunc('No data available in TCC database.')
     
 __artifacts__ = {
     "tcc": (
