20f3985917321e41e24037a20ad524547660a9c1
KefreR <110716132+KefreR@users.noreply.github.com>
2023-11-09 17:08:08 +0100

    Update twint.py

--- scripts/artifacts/twint.py
+++ scripts/artifacts/twint.py
@@ -3,6 +3,20 @@
 from scripts.ilapfuncs import logfunc, logdevinfo, timeline, kmlgen, tsv, is_platform_windows, open_sqlite_db_readonly
 from scripts.artifact_report import ArtifactHtmlReport
 
+
+__artifacts__ = {
+    "Twint": {
+        "name": "Twint Transaction Artifacts",
+        "description": "Collect all the data available related to transactions made with the instant payment app Twint prepaid",
+        "author": "Frank Ressat @KefreR",
+        "version": "0.0.1",
+        "date": "2023-11-09",
+        "paths": ('*/var/mobile/Containers/Data/Application/*/Library/Application Support/Twint.sqlite'),
+        "function": "get_twint"
+    }
+}
+
+
 def get_twint(files_found, report_folder, seeker, wrap_text):
     for file_found in files_found:
         file_found = str(file_found)
@@ -47,9 +61,9 @@ def get_twint(files_found, report_folder, seeker, wrap_text):
         report.add_script()
         data_headers = (
             'Index', 'Creation date', 'Sender confirmation date', 'Receiver validation date', 'Transaction expiry date',
-            'Merchant branch name','Merchant name', 'Sender mobile number', 'Sender message', 'Receiver mobiler number', 
+            'Merchant branch name','Merchant name', 'Sender mobile number', 'Sender message', 'Receiver mobile number', 
             'Receiver contact name', 'Response message', 'Amount authorized for the transaction', 'Paid amount', 
-            'Requested amount', 'Discount', 'Currency', 'Content reference', 'Order link','Presence of multimedia content',
+            'Requested amount', 'Discount', 'Currency', 'Content reference', 'Order link', 'Presence of multimedia content',
             'Transaction status', 'Type of transaction', 'Direction of the transaction', 'Merchant confirmation' )
         report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
         report.end_artifact_report()
@@ -58,10 +72,3 @@ def get_twint(files_found, report_folder, seeker, wrap_text):
         tsv(report_folder, data_headers, data_list, tsvname)
     else:
         logfunc('Twint - No data available')
-
-__artifacts__ = {
-    "twint": (
-        "Twint",
-        ('*/var/mobile/Containers/Data/Application/*/Library/Application Support/Twint.sqlite'),
-        get_twint)
-}
