c2f4a3ea57f0501b13a463c437861e45d1826d2e
KefreR <110716132+KefreR@users.noreply.github.com>
2023-11-27 16:55:01 +0100

    Update twint.py
    Had an issue caused by the number of colunm extracted from the sqlite (n) and the report (n-1) being different. Pointed out by @Johann-PLW when testing with an iPhone backup. Corrected the issue.

--- scripts/artifacts/twint.py
+++ scripts/artifacts/twint.py
@@ -49,11 +49,10 @@ def get_twint(files_found, report_folder, seeker, wrap_text, time_offset):
         ZTRANSACTION.ZDISCOUNT, 
         ZTRANSACTION.ZCURRENCY,
         ZTRANSACTION.ZCONTENTREFERENCE, 
-        ZTRANSACTION.ZORDERLINK, 
-        ZTRANSACTION.ZCONTENTREFERENCESOURCEVALUE, 
+        ZTRANSACTION.ZORDERLINK,
+        ZTRANSACTION.ZP2PHASPICTURE,
         ZTRANSACTION.ZORDERSTATEVALUE,
         ZTRANSACTION.ZORDERTYPEVALUE,
-        ZTRANSACTION.ZP2PHASPICTURE,
         ZTRANSACTION.ZTRANSACTIONSIDEVALUE,
         ZTRANSACTION.ZMERCHANTCONFIRMATION FROM ZTRANSACTION''')
 
@@ -70,7 +69,7 @@ def get_twint(files_found, report_folder, seeker, wrap_text, time_offset):
             'Merchant branch name','Merchant name', 'Sender mobile number', 'Sender message', 'Receiver mobile number', 
             'Receiver contact name', 'Response message', 'Amount authorized for the transaction', 'Paid amount', 
             'Requested amount', 'Discount', 'Currency', 'Content reference', 'Order link', 'Presence of multimedia content',
-            'Transaction status', 'Type of transaction', 'Direction of the transaction', 'Merchant confirmation' )
+            'Transaction status', 'Type of transaction', 'Direction of the transaction', 'Merchant confirmation')
         report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
         report.end_artifact_report()
 
