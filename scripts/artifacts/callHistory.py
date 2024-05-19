256933d10a9a882ec05e429afe026ad6c8f14963
Mori19 <55312081+Mori19@users.noreply.github.com>
2024-05-19 13:28:56 +1000

    Added support for call_history.db
    Old iphones store call history in call_history.db instead of the newer CallHistory.storedata. The schema is a little bit different, so a new sql query was also created.

--- scripts/artifacts/callHistory.py
+++ scripts/artifacts/callHistory.py
@@ -8,7 +8,7 @@ __artifacts_v2__ = {
         "requirements": "none",
         "category": "Call History",
         "notes": "",
-        "paths": ('**/CallHistory.storedata*',),
+        "paths": ('**/CallHistory.storedata*','**/call_history.db',),
         "function": "get_callHistory"
     }
 }
@@ -22,16 +22,9 @@ from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly, convert_ts_human_to_utc, convert_utc_human_to_timezone, convert_bytes_to_unit
 
 def get_callHistory(files_found, report_folder, seeker, wrap_text, timezone_offset):
-    
-    for file_found in files_found:
-        file_found = str(file_found)
-    
-        if file_found.endswith('.storedata'):
-            break
-    
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
+
+    #call_history.db schema taken from here https://avi.alkalay.net/2011/12/iphone-call-history.html 
+    query1 = '''
     select
     datetime(ZDATE+978307200,'unixepoch'),
     CASE
@@ -65,7 +58,49 @@ def get_callHistory(files_found, report_folder, seeker, wrap_text, timezone_offs
     upper(ZISO_COUNTRY_CODE),
     ZLOCATION
     from ZCALLRECORD
-    ''')
+    '''
+
+    query_old = '''
+    select
+    datetime(date, 'unixepoch'),
+    CASE
+        WHEN datetime(date,'unixepoch') = datetime((date + duration),'unixepoch') then 'No Call Duration'
+        ELSE datetime((date + duration), 'unixepoch')
+    END,
+    'N/A' as ZSERVICE_PROVIDER,
+    CASE
+        WHEN flags&4=4 then 'Phone Call'
+        When flags&16=16 then 'FaceTime Call'
+    END,
+    CASE 
+        WHEN flags=0 then 'Incoming'
+        WHEN flags&1=1 then 'Outgoing'
+    END,
+    address,
+    CASE read
+        WHEN 0 then 'No'
+        WHEN 1 then 'Yes'
+    END,
+    strftime('%H:%M:%S', duration, 'unixepoch'),
+    face_time_data,
+    'N/A' as ZDISCONNECTED_CAUSE,
+    country_code,
+    'N/A' as ZLOCATION
+    from call
+    '''
+
+    for file_found in files_found:
+        file_found = str(file_found)
+    
+        if file_found.endswith('.storedata'):
+            break
+        elif file_found.endswith('.db'):
+            query = query_old
+            break
+    
+    db = open_sqlite_db_readonly(file_found)
+    cursor = db.cursor()
+    cursor.execute(query)
 
     all_rows = cursor.fetchall()
     usageentries = len(all_rows)
