6d102d3683dcf5d5884b14b23cce52671fd696ab
Django Faiola <157513033+djangofaiola@users.noreply.github.com>
2024-05-30 18:57:18 +0200

    Add files via upload

--- scripts/version_info.py
+++ scripts/version_info.py
@@ -41,5 +41,6 @@ ileapp_contributors = [
     ['John Hyla', 'https://bluecrewforensics.com', '@jfhyla', 'https://github.com/snoop168'],
     ['Panos Nakoutis', '', '@4n6equals10', ''],
     ['Ruud Schramp', '', '', 'https://github.com/Schramp'],
-    ['Matt Beers', 'https://www.linkedin.com/in/mattbeersii','','https://github.com/dabeersboys']
+    ['Matt Beers', 'https://www.linkedin.com/in/mattbeersii','','https://github.com/dabeersboys'],
+    ['Django Faiola', 'https://djangofaiola.blogspot.com', '@DjangoFaiola', 'https://github.com/djangofaiola']
 ]
