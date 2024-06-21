6d164ded1a96b2f0ef538341248640613a27bd2e
Scott_Kjr3347 <34526954+ScottKjr3347@users.noreply.github.com>
2024-06-21 00:53:56 -0700

    Update Ph15PeopleandDetFacesNAD.py
    corrected a parser error

--- scripts/artifacts/Ph15PeopleandDetFacesNAD.py
+++ scripts/artifacts/Ph15PeopleandDetFacesNAD.py
@@ -5174,10 +5174,10 @@ def get_ph15peopledetfacesyndpl(files_found, report_folder, seeker, wrap_text, t
                 facecropresourcedata_blob = ''
 
                 # zPerson.ZCONTACTMATCHINGDICTIONARY-PLIST
-                if row[6] is not None:
+                if row[5] is not None:
                     pathto = os.path.join(report_folder, 'zPerson-ContactMatchingDict_' + row[112] + '.plist')
                     with open(pathto, 'ab') as wf:
-                        wf.write(row[6])
+                        wf.write(row[5])
 
                     with open(pathto, 'rb') as f:
                         try:
@@ -5188,31 +5188,32 @@ def get_ph15peopledetfacesyndpl(files_found, report_folder, seeker, wrap_text, t
                             if str(ex).find("does not contain an '$archiver' key") >= 0:
                                 logfunc('plist was Not an NSKeyedArchive ' + row[112])
                             else:
-                                logfunc('Error reading exported plist from zPerson-Contact Matching Dictionary' + row[112])
+                                logfunc(
+                                    'Error reading exported plist from zPerson-Contact Matching Dictionary' + row[112])
 
                 # zFaceCrop.ZRESOURCEDATA-BLOB_JPG
-                if row[14] is not None:
+                if row[13] is not None:
                     pathto = os.path.join(report_folder, 'FaceCropFor_' + row[110] + '.jpg')
                     with open(pathto, 'wb') as file:
-                        file.write(row[14])
+                        file.write(row[13])
                     facecropresourcedata_blob = media_to_html(pathto, files_found, report_folder)
 
                 data_list.append((row[0], row[1], row[2], row[3], row[4],
-                                personcontactmatchingdictionary,
-                                row[6], row[7], row[8], row[9], row[10], row[11], row[12],
-                                facecropresourcedata_blob,
-                                row[14], row[15], row[16], row[17], row[18],
-                                row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27],
-                                row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36],
-                                row[37], row[38], row[39], row[40], row[41], row[42], row[43], row[44], row[45],
-                                row[46], row[47], row[48], row[49], row[50], row[51], row[52], row[53], row[54],
-                                row[55], row[56], row[57], row[58], row[59], row[60], row[61], row[62], row[63],
-                                row[64], row[65], row[66], row[67], row[68], row[69], row[70], row[71], row[72],
-                                row[73], row[74], row[75], row[76], row[77], row[78], row[79], row[80], row[81],
-                                row[82], row[83], row[84], row[85], row[86], row[87], row[88], row[89], row[90],
-                                row[91], row[92], row[93], row[94], row[95], row[96], row[97], row[98], row[99],
-                                row[100], row[101], row[102], row[103], row[104], row[105], row[106], row[107],
-                                row[108], row[109], row[110], row[111], row[112], row[113], row[114]))
+                                  personcontactmatchingdictionary,
+                                  row[6], row[7], row[8], row[9], row[10], row[11], row[12],
+                                  facecropresourcedata_blob,
+                                  row[14], row[15], row[16], row[17], row[18],
+                                  row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27],
+                                  row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36],
+                                  row[37], row[38], row[39], row[40], row[41], row[42], row[43], row[44], row[45],
+                                  row[46], row[47], row[48], row[49], row[50], row[51], row[52], row[53], row[54],
+                                  row[55], row[56], row[57], row[58], row[59], row[60], row[61], row[62], row[63],
+                                  row[64], row[65], row[66], row[67], row[68], row[69], row[70], row[71], row[72],
+                                  row[73], row[74], row[75], row[76], row[77], row[78], row[79], row[80], row[81],
+                                  row[82], row[83], row[84], row[85], row[86], row[87], row[88], row[89], row[90],
+                                  row[91], row[92], row[93], row[94], row[95], row[96], row[97], row[98], row[99],
+                                  row[100], row[101], row[102], row[103], row[104], row[105], row[106], row[107],
+                                  row[108], row[109], row[110], row[111], row[112], row[113], row[114]))
 
                 counter += 1
 
