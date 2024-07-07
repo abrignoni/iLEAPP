7d2e1595ed81bc4ad8bf8fae11cdaaa535404eee
Alexis Bringoni <abrignoni@gmail.com>
2024-07-07 09:28:55 -0400

    Update messageRetention.py
    Added support for multiple plists

--- scripts/artifacts/messageRetention.py
+++ scripts/artifacts/messageRetention.py
@@ -7,25 +7,28 @@ from scripts.ilapfuncs import logfunc, logdevinfo, tsv, is_platform_windows
 
 def get_messageRetention(files_found, report_folder, seeker, wrap_text, timezone_offset):
     data_list = []
-    file_found = str(files_found[0])
-    with open(file_found, "rb") as fp:
-        pl = plistlib.load(fp)
-        for key, val in pl.items():
-            
-            if key == 'KeepMessageForDays':
-                data_list.append(('Keep Message for Days', val))
-                logdevinfo(f"<b>Keep Message for Days: </b>{val}")
-            
-        if len(data_list) == 0:
-            data_list.append(('Keep Message for Days', 'Forever'))
-            logdevinfo(f"<b>Keep Message for Days: </b>Forever")
+    
+    for file_found in files_found:
+        with open(file_found, "rb") as fp:
+            pl = plistlib.load(fp)
+            indicator = 0
+            for key, val in pl.items():
+                
+                if key == 'KeepMessageForDays':
+                    data_list.append(('Keep Message for Days',val,file_found))
+                    logdevinfo(f"<b>Keep Message for Days: </b>{val}")
+                    indicator = 1
+                
+            if indicator == 0:
+                data_list.append(('Keep Message for Days','Forever',file_found))
+                logdevinfo(f"<b>Keep Message for Days: </b>Forever")
             
     if len(data_list) > 0:
         report = ArtifactHtmlReport('iOS Message Retention')
         report.start_artifact_report(report_folder, 'iOS Message Retention')
         report.add_script()
-        data_headers = ('Key','Values' )     
-        report.write_artifact_data_table(data_headers, data_list, file_found)
+        data_headers = ('Key','Values','Path')
+        report.write_artifact_data_table(data_headers, data_list, 'File path in the report below')
         report.end_artifact_report()
         
         tsvname = 'iOS Message Retention'
@@ -36,6 +39,6 @@ def get_messageRetention(files_found, report_folder, seeker, wrap_text, timezone
 __artifacts__ = {
     "messageRetention": (
         "Identifiers",
-        ('*/mobile/Library/Preferences/com.apple.MobileSMS.plist'),
+        ('*/com.apple.MobileSMS.plist','*/com.apple.mobileSMS.plist'),
         get_messageRetention)
 }
