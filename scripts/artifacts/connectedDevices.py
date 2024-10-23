32e5f678c0db96ea2b004c9b242c1785ba4764d8
James Habben <james@wmif.net>
2024-10-23 10:52:04 -0700

    update conDev
    - renamed module file
    - updated to __artifacts_v2__
    - updated to @artifact_processor
    - separated user and computer names into individual columns
    - added file offset information for each extracted name pair
    - Included source filename in the output

--- scripts/artifacts/connectedDevices.py
+++ scripts/artifacts/connectedDevices.py
@@ -1,68 +1,75 @@
-import glob
-import os
-import pathlib
-import plistlib
-import sqlite3
-
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, is_platform_windows 
-
-def conDev(files_found, report_folder, seeker, wrap_text, timezone_offset):
-    file_found = str(files_found[0])
-    with open(file_found, "rb") as f:
-        data = f.read()
-        userComps = ""
-
-        logfunc("Data being interpreted for FRPD is of type: " + str(type(data)))
-        x = type(data)
-        byteArr = bytearray(data)
-        userByteArr = bytearray()
-
-        magicOffset = byteArr.find(b"\x01\x01\x80\x00\x00")
-        magic = byteArr[magicOffset : magicOffset + 5]
-
-        flag = 0
-
-        if magic == b"\x01\x01\x80\x00\x00":
-            logfunc(
-                "Found magic bytes in iTunes Prefs FRPD... Finding Usernames and Desktop names now"
-            )
-            for x in range(int(magicOffset + 92), len(data)):
-                if (data[x]) == 0:
-                    x = int(magicOffset) + 157
-                    if userByteArr.decode() == "":
-                        continue
-                    else:
-                        if flag == 0:
-                            userComps += userByteArr.decode() + " - "
-                            flag = 1
-                        else:
-                            userComps += userByteArr.decode() + "\n"
-                            flag = 0
-                        userByteArr = bytearray()
-                        continue
-                else:
-                    char = data[x]
-                    userByteArr.append(char)
-
-    report = ArtifactHtmlReport('Connected Devices')
-    report.start_artifact_report(report_folder, 'Connected Devices')
-    report.add_script()
-    data_list = []
-    data_list.append((userComps,))
-    data_headers = ('User & Computer Names', )     
-    report.write_artifact_data_table(data_headers, data_list, file_found)
-    report.end_artifact_report()
-    
-    tsvname = 'Connected Devices'
-    tsv(report_folder, data_headers, data_list, tsvname)
-    
-    return      
-    
-__artifacts__ = {
-    "condev": (
-        "Connected to",
-        ('**/iTunes_Control/iTunes/iTunesPrefs'),
-        conDev)
+__artifacts_v2__ = {
+    "conDev": {
+        "name": "Connected Devices",
+        "description": "Extracts information about connected devices from iTunes preferences",
+        "author": "",
+        "version": "1.0",
+        "date": "2024-10-23",
+        "requirements": "none",
+        "category": "Connected Devices",
+        "notes": "",
+        "paths": ('*/iTunes_Control/iTunes/iTunesPrefs',),
+        "output_types": "standard"
+    }
 }
-    
+
+from scripts.ilapfuncs import logfunc, artifact_processor
+import os
+
+MAGIC_BYTES = b"\x01\x01\x80\x00\x00"
+MAGIC_OFFSET = 92
+NAME_OFFSET = 157
+
+@artifact_processor
+def conDev(files_found, report_folder, seeker, wrap_text, timezone_offset):
+    data_list = []
+    data_headers = ('User Name', 'Computer Name', 'File Offset', 'Source File')
+    source_path = ''
+
+    for file_found in files_found:
+        source_path = file_found
+        with open(file_found, "rb") as f:
+            data = f.read()
+
+        logfunc(f"Data being interpreted for FRPD is of type: {type(data)}")
+        
+        magic_index = data.find(MAGIC_BYTES)
+        if magic_index == -1:
+            logfunc("Magic bytes not found in iTunes Prefs FRPD")
+            continue
+
+        logfunc("Found magic bytes in iTunes Prefs FRPD... Finding Usernames and Desktop names now")
+        
+        names = []
+        current_name = bytearray()
+        name_start_offset = magic_index + MAGIC_OFFSET
+        for i, byte in enumerate(data[name_start_offset:]):
+            if byte == 0:
+                if current_name:
+                    names.append((current_name.decode(), name_start_offset + i - len(current_name)))
+                    current_name = bytearray()
+                    name_start_offset = name_start_offset + i + 1
+            else:
+                current_name.append(byte)
+
+        # Process names in pairs
+        for i in range(0, len(names), 2):
+            if i + 1 < len(names):
+                user_name, user_offset = names[i]
+                computer_name, _ = names[i+1]
+                data_list.append((
+                    user_name,
+                    computer_name,
+                    str(user_offset),
+                    os.path.basename(file_found)
+                ))
+            else:
+                user_name, user_offset = names[i]
+                data_list.append((
+                    user_name,
+                    '',
+                    str(user_offset),
+                    os.path.basename(file_found)
+                ))
+
+    return data_headers, data_list, source_path
