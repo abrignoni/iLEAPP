5db2719b64ed9405571d73070a12b183977c6ea8
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2024-10-24 21:14:43 -0400

    Update DataUsage.py

--- scripts/artifacts/DataUsage.py
+++ scripts/artifacts/DataUsage.py
@@ -1,5 +1,5 @@
 __artifacts_v2__ = {
-    "datausage": {
+    "get_DataUsage": {
         "name": "Data Usage",
         "description": "Parses application network data usage",
         "author": "@KevinPagano3",
@@ -9,16 +9,21 @@ __artifacts_v2__ = {
         "category": "Network Usage",
         "notes": "",
         "paths": ('*/wireless/Library/Databases/DataUsage.sqlite*',),
+        "output_types": ["html", "tsv", "timeline", "lava"],
         "function": "get_DataUsage"
     }
 }
 
 import sqlite3
 
-from scripts.artifact_report import ArtifactHtmlReport
+from scripts.ilapfuncs import artifact_processor
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly, convert_ts_human_to_utc, convert_utc_human_to_timezone
 
+@artifact_processor
 def get_DataUsage(files_found, report_folder, seeker, wrap_text, timezone_offset):
+    
+    data_list = []
+    
     for file_found in files_found:
         file_found = str(file_found)
         
@@ -46,33 +51,22 @@ def get_DataUsage(files_found, report_folder, seeker, wrap_text, timezone_offset
             ''')
 
             all_rows = cursor.fetchall()
-            usageentries = len(all_rows)
-            if usageentries > 0:
+            
+            for row in all_rows:
+                firstused = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[0]),timezone_offset)
+                lastused = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[1]),timezone_offset)
+                lastconnected = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[2]),timezone_offset)
                 
-                report = ArtifactHtmlReport('Network Usage (DataUsage) - App Data')
-                report.start_artifact_report(report_folder, 'Network Usage (DataUsage) - App Data')
-                report.add_script()
-                data_headers = ('Last Connect Timestamp','First Usage Timestamp','Last Usage Timestamp','Bundle Name','Process Name','Type','Wifi In (Bytes)','Wifi Out (Bytes)','Mobile/WWAN In (Bytes)','Mobile/WWAN Out (Bytes)') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+                process_split = row[4].split('/')
+                data_list.append((lastconnected,firstused,lastused,row[3],process_split[0],row[5],row[6],row[7],row[8],row[9]))
                 
-                data_list = []
-                for row in all_rows:
-                    firstused = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[0]),timezone_offset)
-                    lastused = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[1]),timezone_offset)
-                    lastconnected = convert_utc_human_to_timezone(convert_ts_human_to_utc(row[2]),timezone_offset)
-                    
-                    process_split = row[4].split('/')
-                    data_list.append((lastconnected,firstused,lastused,row[3],process_split[0],row[5],row[6],row[7],row[8],row[9]))
-                    
-                report.write_artifact_data_table(data_headers, data_list, file_found)
-                report.end_artifact_report()
-                
-                tsvname = 'Network Usage (DataUsage) - App Data'
-                tsv(report_folder, data_headers, data_list, tsvname)
-                
-                tlactivity = 'Network Usage (DataUsage) - App Data'
-                timeline(report_folder, tlactivity, data_list, data_headers)
-            else:
-                logfunc('No Network Usage (DataUsage) - App Data available')
             db.close()
+            
         else:
             continue
+            
+    if not data_list:
+        logfunc('No Network Usage (DataUsage) - App Data available')
+    
+    data_headers = ((('Last Connect Timestamp','datetime'),('First Usage Timestamp','datetime'),('Last Usage Timestamp','datetime'),'Bundle Name','Process Name','Entry Type','Wifi In (Bytes)','Wifi Out (Bytes)','Mobile/WWAN In (Bytes)','Mobile/WWAN Out (Bytes)'))
+    return data_headers, data_list, file_found
