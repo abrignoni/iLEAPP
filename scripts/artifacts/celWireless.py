6354ad6f37b46b84e128396d164ca2401a4ab5f1
James Habben <james@wmif.net>
2024-10-29 14:06:58 -0700

    module updates

--- scripts/artifacts/celWireless.py
+++ scripts/artifacts/celWireless.py
@@ -17,7 +17,7 @@ __artifacts_v2__ = {
 import os
 import plistlib
 
-from scripts.ilapfuncs import logdevinfo, artifact_processor
+from scripts.ilapfuncs import device_info, artifact_processor
 
 @artifact_processor
 def celWireless(files_found, report_folder, seeker, wrap_text, timezone_offset):
@@ -30,15 +30,15 @@ def celWireless(files_found, report_folder, seeker, wrap_text, timezone_offset):
                 for key, val in plist.items():
                     data_list.append((key, str(val), filepath))
                     if key == "ReportedPhoneNumber":
-                        logdevinfo(f"<b>Reported Phone Number: </b>{val}")
+                        device_info("Cellular", "Reported Phone Number", val)
                     elif key == "CDMANetworkPhoneNumberICCID":
-                        logdevinfo(f"<b>CDMA Network Phone Number ICCID: </b>{val}")
+                        device_info("Cellular", "CDMA Network Phone Number ICCID", val)
                     elif key == "imei":
-                        logdevinfo(f"<b>IMEI: </b>{val}")
+                        device_info("Cellular", "IMEI", val)
                     elif key == "LastKnownICCID":
-                        logdevinfo(f"<b>Last Known ICCID: </b>{val}")
+                        device_info("Cellular", "Last Known ICCID", val)
                     elif key == "meid":
-                        logdevinfo(f"<b>MEID: </b>{val}")
+                        device_info("Cellular", "MEID", val)
     
     data_headers = ('Data Key', 'Data Value', 'Source File')
     return data_headers, data_list, filepath
