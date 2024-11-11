9950d2d746d2bf18d88363da557aaea74ce7ae20
Johann Polewczyk <johannplw@me.com>
2024-11-11 18:52:25 +0100

    Upddate serialNumber.py for lava output

--- scripts/artifacts/serialNumber.py
+++ scripts/artifacts/serialNumber.py
@@ -1,60 +1,45 @@
 __artifacts_v2__ = {
     "serialNumber": {
         "name": "Serial Number",
-        "description": "Serial Number",
+        "description": "Serial Number of the device",
         "author": "@AlexisBrignoni",
         "version": "0.2",
-        "date": "2023-11-21",
+        "date": "2023-09-30",
         "requirements": "none",
         "category": "Identifiers",
         "notes": "",
         "paths": ('*/Library/Caches/locationd/consolidated.db*'),
-        "function": "get_serialNumber"
+        "output_types": "none"
     }
 }
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, logdevinfo, tsv, open_sqlite_db_readonly
 
-def get_serialNumber(files_found, report_folder, seeker, wrap_text, timezone_offset):
-    
+from scripts.ilapfuncs import artifact_processor, open_sqlite_db_readonly, device_info
+
+@artifact_processor
+def serialNumber(files_found, report_folder, seeker, wrap_text, timezone_offset):
+    db_file = ''
+
     for file_found in files_found:
-        file_found = str(file_found)
-        
         if file_found.endswith('consolidated.db'):
+            db_file = file_found
             break
     
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    
-    cursor.execute('''
-    SELECT
-    SerialNumber
-    FROM TableInfo
-    ''')
-    
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    
-    if usageentries > 0:
-        data_list =[]
+    if db_file:
+        db = open_sqlite_db_readonly(file_found)
+        cursor = db.cursor()
+        
+        cursor.execute('''
+        SELECT DISTINCT SerialNumber
+        FROM TableInfo
+        ''')
+        
+        all_rows = cursor.fetchall()
+
         for row in all_rows:
-            data_list.append((row[0],))
-            logdevinfo(f"<b>Serial Number: </b>{row[0]}")
-            break
-            
-        description = 'Serial Number'
-        report = ArtifactHtmlReport('Serial Number')
-        report.start_artifact_report(report_folder, 'Serial Number', description)
-        report.add_script()
-        data_headers = ('Serial Number',)
-        report.write_artifact_data_table(data_headers, data_list, file_found, html_escape=False)
-        report.end_artifact_report()
-        
-        tsvname = f'Serial Number'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-    else:
-        logfunc('No Serial Number available in consolidated.db')
-        
-    db.close()
+            device_info("Device Information", "Serial Number", row[0], db_file)
+
+        db.close()
+                
+    # Return empty data since this artifact only collects device info
+    return (), [], db_file
