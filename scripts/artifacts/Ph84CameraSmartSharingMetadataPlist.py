1ba4f34b1dae10c40980ed8d2141f4d781a96027
Scott_Kjr3347 <34526954+ScottKjr3347@users.noreply.github.com>
2025-01-06 23:05:47 -0800

    Updated for LAVA and ios17-18 updates
    Photos.sqlite Parsers updated for LAVA tool.
    Updates for iOS 17.6 schema change and GA iOS 18 schema
    Developer betas not supported.
    working on errors for plist parsers but data is included.
    updated tested on FFS, Adv Log, Log + ios 14-ios 18

--- scripts/artifacts/Ph84CameraSmartSharingMetadataPlist.py
+++ scripts/artifacts/Ph84CameraSmartSharingMetadataPlist.py
@@ -1,44 +1,55 @@
-# Author:  Scott Koenig https://theforensicscooter.com/
-# Version: 1.0
-#
-#   Description:
-#   Parses basic data from */PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist
-#   which contains some important data related to iCloud Shared Photos Library
-#   Smart Camera Settings and auto sharing. Additional information and
-#   explanation of some keys-fields might be found with research and published blogs written by
-#   Scott Koenig https://theforensicscooter.com/
+__artifacts_v2__ = {
+    'Ph84CameraSmartSharingMetadataPlist': {
+        'name': 'Ph84-Camera-Smart-Sharing-Metadata-Plist',
+        'description': 'Parses basic data from */PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist'
+                       ' which contains some important data related to iCloud Shared Photos Library'
+                       ' Smart Camera Settings and auto sharing. Additional information and'
+                       ' explanation of some keys-fields might be found with research and published blogs written by'
+                       ' Scott Koenig https://theforensicscooter.com/',
+        'author': 'Scott Koenig',
+        'version': '5.0',
+        'date': '2025-01-05',
+        'requirements': 'Acquisition that contains camera_smart_sharing_metadata.plist',
+        'category': 'Photos-Z-Settings',
+        'notes': '',
+        'paths': ('*/PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist',),
+        "output_types": ["standard", "tsv", "none"]
+    }
+}
 
+import datetime
 import os
 import plistlib
-import biplist
 import nska_deserialize as nd
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, logdevinfo, tsv, is_platform_windows 
+import scripts.artifacts.artGlobals
+from scripts.builds_ids import OS_build
+from scripts.ilapfuncs import artifact_processor, logfunc, device_info, get_file_path
 
-
-def get_ph84camerasmartsharingmetadataplist(files_found, report_folder, seeker, wrap_text, timezone_offset):
+@artifact_processor
+def Ph84CameraSmartSharingMetadataPlist(files_found, report_folder, seeker, wrap_text, timezone_offset):
     data_list = []
-    file_found = str(files_found[0])
-    if file_found.endswith('camera_smart_sharing_metadata.plist'):
-        with open(file_found, 'rb') as f:
+    source_path = str(files_found[0])
+
+    if source_path.endswith('camera_smart_sharing_metadata.plist'):
+        with open(source_path, 'rb') as f:
             try:
                 deserialized_plist = nd.deserialize_plist(f)
                 plist = deserialized_plist
-                for key, value in plist.items():
-                    if key == 'homeLocations':
-                        homelocations = value
+                for key, val in plist.items():
                     if key == 'creationDate':
-                        creationdate = value
+                        creationdate = val
+                    if key == 'homeLocations':
+                        homelocations = val
                     if key == 'frequentLocations':
-                        frequentlocations = value
+                        frequentlocations = val
                     if key == 'identities':
-                        identities = value
+                        identities = val
                     if key == 'locationShiftingRequired':
-                        locationshiftingrequired = value
+                        locationshiftingrequired = val
                     if key == 'version':
-                        version = value
+                        version = val
                     if key == 'libraryScopeLocalIdentifier':
-                        libraryscopelocalidentifier = value
+                        libraryscopelocalidentifier = val
 
             except (nd.DeserializeError,
                     nd.biplist.NotBinaryPlistException,
@@ -49,39 +60,17 @@ def get_ph84camerasmartsharingmetadataplist(files_found, report_folder, seeker, 
                     TypeError, OSError, OverflowError) as ex:
                 logfunc('Had exception: ' + str(ex))
 
-        data_list.append((homelocations, creationdate, frequentlocations, identities, locationshiftingrequired,
+        data_list.append((creationdate, homelocations, frequentlocations, identities, locationshiftingrequired,
                           version, libraryscopelocalidentifier))
 
-        description = ('Parses basic data from */PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist'
-                       ' which contains some important data related to iCloud Shared Photos Library Smart'
-                       ' Camera Settings and auto sharing.')
-        report = ArtifactHtmlReport('Ph84-Camera-Smart-Sharing-Metadata-Plist')
-        report.start_artifact_report(report_folder, 'Ph84-Camera-Smart-Sharing-Metadata-Plist', description)
-        report.add_script()
-        data_headers = ('homeLocations', 'creationDate', 'frequentLocations', 'identities', 'locationShiftingRequired',
-                        'version', 'libraryScopeLocalIdentifier')
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-
-        tsvname = 'Ph84-Camera-Smart-Sharing-Metadata-Plist'
-        tsv(report_folder, data_headers, data_list, tsvname)
+    data_headers = (
+        'creationdate',
+        'homelocations',
+        'frequentlocations',
+        'identities',
+        'locationshiftingrequired',
+        'version',
+        'libraryscopelocalidentifier')
+    return data_headers, data_list, source_path
 
 
-__artifacts_v2__ = {
-    'Ph84-Camera-Smart-Sharing-Metadata-Plist': {
-        'name': 'Camera Smart Sharing Settings Ph84 Camera Smart Sharing Metadata Plist',
-        'description': 'Parses basic data from */PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist'
-                       ' which contains some important data related to iCloud Shared Photos Library'
-                       ' Smart Camera Settings and auto sharing. Additional information and'
-                       ' explanation of some keys-fields might be found with research and published blogs written by'
-                       ' Scott Koenig https://theforensicscooter.com/',
-        'author': 'Scott Koenig https://theforensicscooter.com/',
-        'version': '1.0',
-        'date': '2024-06-8',
-        'requirements': 'Acquisition that contains camera_smart_sharing_metadata.plist',
-        'category': 'Photos-Z-Settings',
-        'notes': '',
-        'paths': '*/PhotoData/Caches/SmartSharing/camera_smart_sharing_metadata.plist',
-        'function': 'get_ph84camerasmartsharingmetadataplist'
-    }
-}
