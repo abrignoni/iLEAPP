fd8e23c724ea6d3dddc9ada16c62963f3ba6765f
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2025-03-27 14:48:58 -0400

    System Info/Build Updates

--- scripts/artifacts/lastBuild.py
+++ scripts/artifacts/lastBuild.py
@@ -2,13 +2,14 @@ __artifacts_v2__ = {
     "lastBuild": {
         "name": "iOS Information",
         "description": "Extract iOS information from the LastBuildInfo.plist file",
-        "author": "@AlexisBrignoni - @ydkhatri",
-        "version": "0.5.4",
-        "date": "2020-04-30",
+        "author": "@AlexisBrignoni - @ydkhatri - @stark4n6",
+        "version": "0.5.5",
+        "creation_date": "2020-04-30",
+        "last_update_date": "2025-03-27",
         "requirements": "none",
         "category": "IOS Build",
         "notes": "",
-        "paths": ('*/installd/Library/MobileInstallation/LastBuildInfo.plist',),
+        "paths": ('*/installd/Library/MobileInstallation/LastBuildInfo.plist','*/logs/SystemVersion/SystemVersion.plist'),
         "output_types": ["html", "tsv", "lava"],
         "artifact_icon": "git-commit"
     }
@@ -20,23 +21,48 @@ from scripts.ilapfuncs import artifact_processor, get_file_path, get_plist_file_
 
 @artifact_processor
 def lastBuild(files_found, report_folder, seeker, wrap_text, time_offset):
-    source_path = get_file_path(files_found, "LastBuildInfo.plist")
-    data_list = []
-    
-    pl = get_plist_file_content(source_path)
-    for key, val in pl.items():
-        data_list.append((key, val))
-        if key == ("ProductVersion"):
-            scripts.artifacts.artGlobals.versionf = val
-            logfunc(f"iOS version: {val}")
-            device_info("Device Information", "iOS version", val, source_path)
-        
-        if key == "ProductBuildVersion":
-            device_info("Device Information", "ProductBuildVersion", val, source_path)
-        
-        if key == ("ProductName"):
-            logfunc(f"Product: {val}")
-            device_info("Device Information", "Product Name", val, source_path)
+    for file_found in files_found:
+        file_found = str(file_found)
+        if file_found.endswith('LastBuildInfo.plist'):
+            source_path = get_file_path(files_found, "LastBuildInfo.plist")
+            data_list = []
+            
+            pl = get_plist_file_content(source_path)
+            for key, val in pl.items():
+                data_list.append((key, val))
+                if key == ("ProductVersion"):
+                    scripts.artifacts.artGlobals.versionf = val
+                    logfunc(f"iOS version: {val}")
+                    device_info("Device Information", "iOS version", val, source_path)
+                
+                if key == "ProductBuildVersion":
+                    device_info("Device Information", "ProductBuildVersion", val, source_path)
+                
+                if key == ("ProductName"):
+                    logfunc(f"Product: {val}")
+                    device_info("Device Information", "Product Name", val, source_path)
 
-    data_headers = ('Property','Property Value' )     
-    return data_headers, data_list, source_path
+            data_headers = ('Property','Property Value' )     
+            return data_headers, data_list, source_path
+            
+        elif file_found.endswith('SystemVersion.plist'):
+            source_path = get_file_path(files_found, "SystemVersion.plist")
+            data_list = []
+            
+            pl = get_plist_file_content(source_path)
+            for key, val in pl.items():
+                data_list.append((key, val))
+                if key == ("ProductVersion"):
+                    scripts.artifacts.artGlobals.versionf = val
+                    logfunc(f"iOS version: {val}")
+                    device_info("Device Information", "iOS version", val, source_path)
+                
+                if key == "ProductBuildVersion":
+                    device_info("Device Information", "ProductBuildVersion", val, source_path)
+                
+                if key == ("ProductName"):
+                    logfunc(f"Product: {val}")
+                    device_info("Device Information", "Product Name", val, source_path)
+
+            data_headers = ('Property','Property Value' )     
+            return data_headers, data_list, source_path
