c6032359584364b2577eb2a30ab910fa764a546d
Johann POLEWCZYK <johannplw@me.com>
2025-05-22 08:04:21 +0200

    Add logarchive_artifacts

--- scripts/artifacts/logarchive.py
+++ scripts/artifacts/logarchive.py
@@ -6,25 +6,37 @@ __artifacts_v2__ = {
         "creation_date": "2025-05-06",
         "last_update_date": "2025-05-06",
         "requirements": "none",
-        "category": "Logs",
+        "category": "Unified Logs",
         "notes": "",
         "paths": ('*/logarchive.json',),
-        "output_types": "none",
+        "output_types": "lava_only",
         "artifact_icon": "database",
-        "function": "logarchive"
+    },
+    "logarchive_artifacts": {
+        "name": "logarchive artifacts",
+        "description": "Extract relevant entries from the logarchive table of LAVA db",
+        "author": "@AlexisBrignoni, @JohannPLW",
+        "creation_date": "2025-05-19",
+        "last_update_date": "2025-05-21",
+        "requirements": "logarchive module must be executed first",
+        "category": "Unified Logs",
+        "notes": "",
+        "paths": None,
+        "output_types": "lava_only",
+        "artifact_icon": "database",
     }
 }
 
 import ijson
 from datetime import datetime, timezone
-from scripts.lavafuncs import lava_process_artifact, lava_insert_sqlite_data
-from scripts.ilapfuncs import get_file_path, get_plist_file_content, logfunc
+from scripts.ilapfuncs import artifact_processor, get_file_path, get_sqlite_db_records, logfunc
+
 
 def convert_to_utc(timestamp):
     dt_local = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f%z")
     dt_utc = dt_local.astimezone(timezone.utc)
-    
-    return(dt_utc)
+
+    return dt_utc
 
 
 def truncate_after_last_bracket(file_path):
@@ -32,7 +44,7 @@ def truncate_after_last_bracket(file_path):
         # Start from the end of the file and scan backwards
         f.seek(0, 2)  # Move to end of file
         file_size = f.tell()
-        
+
         for i in range(file_size - 1, -1, -1):
             f.seek(i)
             char = f.read(1)
@@ -43,23 +55,21 @@ def truncate_after_last_bracket(file_path):
                 return
         print("No closing bracket `]` found.")
 
+@artifact_processor
 def logarchive(files_found, report_folder, seeker, wrap_text, timezone_offset):
-    
+    source_path = get_file_path(files_found, 'logarchive.json')
     data_list = []
-    data_list_artifacts = []
-    
-    for file_found in files_found:
-        file_found = str(file_found)
-    
+
     incval = 0
-    if file_found.endswith('logarchive.json'):
-        truncate_after_last_bracket(file_found)
-        with open(file_found, 'rb') as f:
+    if source_path:
+        truncate_after_last_bracket(source_path)
+        with open(source_path, 'rb') as f:
             for record in ijson.items(f, 'item', multiple_values=True ): # if the json is a list
                 if isinstance(record, dict):
-                    timestamp = processid = subsystem = category = eventmessage = traceid = ''
+                    timestamp = processid = process_image_path = subsystem = ''
+                    category = eventmessage = traceid = ''
                     incval = incval + 1
-                    
+
                     for key, value in record.items():
                         if key == 'timestamp':
                             value = convert_to_utc(value)
@@ -67,7 +77,7 @@ def logarchive(files_found, report_folder, seeker, wrap_text, timezone_offset):
                         elif key == 'processID':
                             processid = value
                         elif key == 'processImagePath':
-                            processImagePath = value
+                            process_image_path = value
                         elif key == 'subsystem':
                             subsystem = value
                         elif key == 'category':
@@ -77,66 +87,69 @@ def logarchive(files_found, report_folder, seeker, wrap_text, timezone_offset):
                         elif key == 'traceID':
                             traceid = str(value)
 
-                    #print(timestamp, processid, subsystem, category, eventmessage, traceid)
                     
-                    data_list.append((timestamp, incval, processImagePath, processid, subsystem, category, eventmessage, traceid))
+                    data_list.append((timestamp, incval, process_image_path, processid, subsystem,
+                                      category, eventmessage, traceid))
 
-                    #Identification and aggregation of relevant artifacts
-                    if ('Take screenshot' in eventmessage or
-                        'charger connected state change' in eventmessage or
-                        'Motion State Transition:' in eventmessage or
-                        'CarPlay Connection Event:' in eventmessage or
-                        'CoreAnalytics event: com.apple.accessories.connection.added' in eventmessage or
-                        'Start #SpeechRequest id' in eventmessage or
-                        'Received Orientation' in eventmessage or
-                        'Effective device orientation' in eventmessage or
-                        'Received: Face' in eventmessage or
-                        'AppleAccount Authenticated:' in eventmessage or
-                        '=> Transitioning to state:' in eventmessage or
-                        'Received: Screen' in eventmessage or
-                        'Screen did lock' in eventmessage or
-                        'ScreenOn changed' in eventmessage or
-                        'Screen shut off' in eventmessage or
-                        'screen is locked' in eventmessage or
-                        'screen is unlocked' in eventmessage or
-                        'Device unlocked' in eventmessage or
-                        'Device lock status' in eventmessage or
-                        'Biometric match complete' in eventmessage or
-                        'SBIconView touches began with event:' in eventmessage or
-                        'Setting process visibility' in eventmessage or
-                        'WiFi state changed:' in eventmessage or
-                        'Toggled WiFi state' in eventmessage or
-                        'is WiFi associated?' in eventmessage or
-                        'ForgetSSID' in eventmessage or
-                        'en0: SSID' in eventmessage or
-                        'Removing Lease SSID' in eventmessage or
-                        'SysMon: WiFi state changed:' in eventmessage or
-                        'WiFiDeviceManagerSetNetworks:' in eventmessage or 
-                        'Scanning For Broadcast found:' in eventmessage or
-                        'Scanning Remaining Channels' in eventmessage or
-                        'WiFiSettlementObserver _handleScanResults' in eventmessage or
-                        'Attempting to join' in eventmessage or
-                        'Preparing background scan request for ' in eventmessage or
-                        'WiFiNetworkPrepareKnownBssList' in eventmessage or
-                        'to list of known networks' in eventmessage or
-                        '{AUTOJOIN, SCAN*} Scanning 2Ghz Channels found:' in eventmessage or
-                        '{AUTOJOIN, SCAN*} Scanning 5Ghz Channels found:' in eventmessage or
-                        'is asking to connect device' in eventmessage or
-                        'ATXModeDrivingFeaturizer: Driving mode' in eventmessage or
-                        'ATXModeCorrelatedAppsDataSource: user' in eventmessage):
-                        data_list_artifacts.append((timestamp, incval, processImagePath, processid, subsystem, category, eventmessage, traceid))
-                
-    #Create LAVA tables
-    category = "Logs"
-    module_name = "logarchive"
-        
-    data_headers = (('Timestamp', 'datetime'), 'Row Number', 'Process Image Path', 'Process ID', 'Subsystem', 'Category', 'Event Message', 'Trace ID')
-    table_name1, object_columns1, column_map1 = lava_process_artifact(category, module_name, 'Log Archive', data_headers, len(data_list))
-    lava_insert_sqlite_data(table_name1, data_list, object_columns1, data_headers, column_map1)
-    
-    
-    data_headers_artifacts = (('Timestamp', 'datetime'), 'Row Number', 'Process Image Path', 'Process ID', 'Subsystem', 'Category', 'Event Message', 'Trace ID')
-    table_name2, object_columns2, column_map2 = lava_process_artifact(category, module_name, 'Log Archive Artifacts', data_headers_artifacts, len(data_list_artifacts))
-    lava_insert_sqlite_data(table_name2, data_list_artifacts, object_columns2, data_headers_artifacts, column_map2)
+    data_headers = (('Timestamp', 'datetime'), 'Row Number', 'Process Image Path', 'Process ID',
+                    'Subsystem', 'Category', 'Event Message', 'Trace ID')
+    return data_headers, data_list, source_path
 
-    
+@artifact_processor
+def logarchive_artifacts(files_found, report_folder, seeker, wrap_text, timezone_offset):
+    source_path = get_file_path(files_found, '_lava_artifacts.db')
+    data_list = []
+
+    query = '''
+    SELECT *
+    FROM logarchive
+    WHERE event_message LIKE '%Take screenshot%'
+        OR event_message LIKE '%Take screenshot%'
+        OR event_message LIKE '%charger connected state change%'
+        OR event_message LIKE '%Motion State Transition:%'
+        OR event_message LIKE '%CarPlay Connection Event:%'
+        OR event_message LIKE '%CoreAnalytics event: com.apple.accessories.connection.added%'
+        OR event_message LIKE '%Start #SpeechRequest id%'
+        OR event_message LIKE '%Received Orientation%'
+        OR event_message LIKE '%Effective device orientation%'
+        OR event_message LIKE '%Received: Face%'
+        OR event_message LIKE '%AppleAccount Authenticated:%'
+        OR event_message LIKE '%=> Transitioning to state:%'
+        OR event_message LIKE '%Received: Screen%'
+        OR event_message LIKE '%Screen did lock%'
+        OR event_message LIKE '%ScreenOn changed%'
+        OR event_message LIKE '%Screen shut off%'
+        OR event_message LIKE '%screen is locked%'
+        OR event_message LIKE '%screen is unlocked%'
+        OR event_message LIKE '%Device unlocked%'
+        OR event_message LIKE '%Device lock status%'
+        OR event_message LIKE '%Biometric match complete%'
+        OR event_message LIKE '%SBIconView touches began with event:%'
+        OR event_message LIKE '%Setting process visibility%'
+        OR event_message LIKE '%WiFi state changed:%'
+        OR event_message LIKE '%Toggled WiFi state%'
+        OR event_message LIKE '%is WiFi associated?%'
+        OR event_message LIKE '%ForgetSSID%'
+        OR event_message LIKE '%en0: SSID%'
+        OR event_message LIKE '%Removing Lease SSID%'
+        OR event_message LIKE '%SysMon: WiFi state changed:%'
+        OR event_message LIKE '%WiFiDeviceManagerSetNetworks:%'
+        OR event_message LIKE '%Scanning For Broadcast found:%'
+        OR event_message LIKE '%Scanning Remaining Channels%'
+        OR event_message LIKE '%WiFiSettlementObserver _handleScanResults%'
+        OR event_message LIKE '%Attempting to join%'
+        OR event_message LIKE '%Preparing background scan request for %'
+        OR event_message LIKE '%WiFiNetworkPrepareKnownBssList%'
+        OR event_message LIKE '%to list of known networks%'
+        OR event_message LIKE '%{AUTOJOIN, SCAN*} Scanning 2Ghz Channels found:%'
+        OR event_message LIKE '%{AUTOJOIN, SCAN*} Scanning 5Ghz Channels found:%'
+        OR event_message LIKE '%is asking to connect device%'
+        OR event_message LIKE '%ATXModeDrivingFeaturizer: Driving mode%'
+        OR event_message LIKE '%ATXModeCorrelatedAppsDataSource: user%'
+    '''
+
+    data_list = get_sqlite_db_records(source_path, query)
+    data_headers = (('Timestamp', 'datetime'), 'Row Number', 'Process Image Path', 'Process ID', 
+                    'Subsystem', 'Category', 'Event Message', 'Trace ID')
+
+    return data_headers, data_list, source_path
