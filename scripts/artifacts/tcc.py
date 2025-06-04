56b025852ba18b161f939121050bd5bcce64918c
prosch88 <prosch88@googlemail.com>
2025-06-04 09:24:00 +0200

    modified:   scripts/artifacts/sysShutdown.py 	modified:   scripts/artifacts/sysdiagnose.py 	modified:   scripts/artifacts/tcc.py Added more Sysdiagnose artifacts (ps, df) and modified sysShutdown and tcc to parse archives

--- scripts/artifacts/tcc.py
+++ scripts/artifacts/tcc.py
@@ -2,18 +2,21 @@ __artifacts_v2__ = {
     'tcc': {
         'name': 'Application Permissions',
         'description': 'Extract application permissions from TCC.db database',
-        'author': '@AlexisBrignoni - @KevinPagano3 - @johannplw',
+        'author': '@AlexisBrignoni - @KevinPagano3 - @johannplw - @C_Peter',
         'creation_date': '2020-12-15',
-        'last_update_date': '2025-04-07',
+        'last_update_date': '2025-06-04',
         'requirements': 'none',
         'category': 'App Permissions',
         'notes': '',
-        'paths': ('*/mobile/Library/TCC/TCC.db*',),
+        'paths': ('*/mobile/Library/TCC/TCC.db*','*/logs/Accessibility/TCC.db*','**/sysdiagnose_*.tar.gz'),
         'output_types': 'standard',
         'artifact_icon': 'key'
     }
 }
 
+import tarfile
+import tempfile
+import os
 
 from scripts.ilapfuncs import artifact_processor, \
     get_file_path, get_sqlite_db_records, does_column_exist_in_db, \
@@ -22,7 +25,33 @@ from scripts.ilapfuncs import artifact_processor, \
 
 @artifact_processor
 def tcc(files_found, report_folder, seeker, wrap_text, timezone_offset):
-    source_path = get_file_path(files_found, 'TCC.db')
+    temp_db = None
+    for file_found in files_found:
+        filename = os.path.basename(str(file_found))
+        if filename == "TCC.db":
+            source_path = get_file_path(files_found, filename)
+            report_path = source_path
+            break
+        elif 'sysdiagnose_' in filename and not "IN_PROGRESS_" in filename:
+            report_path = get_file_path(files_found, filename)
+            tar = tarfile.open(report_path)
+            root = tar.getmembers()[0].name.split('/')[0]
+            try:
+                tarf = tar.extractfile(f"{root}/logs/Accessibility/TCC.db")
+                temp_db = tempfile.NamedTemporaryFile(suffix=".db", delete=False)
+                temp_db.write(tarf.read())
+                temp_db.flush()
+                temp_db.close()
+                source_path = temp_db.name
+            except:
+                source_path = get_file_path(files_found, "TCC.db")
+                report_path = source_path
+                continue
+        else:
+            source_path = get_file_path(files_found, "TCC.db")
+            report_path = source_path
+            continue
+
     data_list = []
 
     last_modified_timestamp_exists = does_column_exist_in_db(
@@ -82,4 +111,8 @@ def tcc(files_found, report_folder, seeker, wrap_text, timezone_offset):
             data_list.append((record[1], record[2].replace('kTCCService', ''),
                               record[3], record[4]))
 
-    return data_headers, data_list, source_path
+    if temp_db and os.path.exists(temp_db.name):
+        try: os.remove(temp_db.name)
+        except: pass
+    
+    return data_headers, data_list, report_path
