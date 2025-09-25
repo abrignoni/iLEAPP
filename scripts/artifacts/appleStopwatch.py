32f5980207fa8b5b3dfef3a6b60d89bcadc48438
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-09-25 17:33:10 -0300

    Update stopwatch artifact to use Context object

--- scripts/artifacts/appleStopwatch.py
+++ scripts/artifacts/appleStopwatch.py
@@ -16,10 +16,12 @@ __artifacts_v2__ = {
 
 import datetime
 
-from scripts.ilapfuncs import artifact_processor, get_file_path, get_plist_file_content, convert_plist_date_to_utc
+from scripts.ilapfuncs import artifact_processor, get_file_path, get_plist_file_content
+from scripts.context import Context
 
 @artifact_processor
-def stopwatch(files_found, report_folder, seeker, wrap_text, timezone_offset):
+def stopwatch(context:Context):
+    files_found = context.get_files_found()
     source_path = get_file_path(files_found, "com.apple.mobiletimerd.plist")
     data_list = []
 
