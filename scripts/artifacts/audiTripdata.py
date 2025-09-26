7240ca72d9f2a1a83c307cc408d19f9ef2818ae4
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-09-26 09:53:44 -0300

    Refactor Audi Trip Data artifact
    Updated audiTripdata.py to use the v2 artifact and context object. Removed manual reporting

--- scripts/artifacts/audiTripdata.py
+++ scripts/artifacts/audiTripdata.py
@@ -1,13 +1,28 @@
+__artifacts_v2__ = {
+    "get_audiTripdata": {
+        "name": "Audi Trip Data",
+        "description": "",
+        "author": "@AuthorUsername",
+        "creation_date": "2024-06-01",
+        "last_update_date": "2025-04-13",
+        "requirements": "none",
+        "category": "Audi Trips",
+        "notes": "",
+        "paths": ('*/Library/Caches/de.audi.myaudimobileassistant/fsCachedData/**',),
+        "output_types": "standard"
+    }
+}
+
 import json
 import os
 
-from packaging import version
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, logdevinfo, timeline, tsv, is_platform_windows, convert_utc_human_to_timezone, convert_ts_human_to_utc
+from scripts.ilapfuncs import convert_ts_human_to_utc, artifact_processor
+from scripts.context import Context
 
-def get_audiTripdata(files_found, report_folder, seeker, wrap_text, timezone_offset):
+@artifact_processor
+def get_audiTripdata(context:Context):
 	data_list = []
-	for file_found in files_found:
+	for file_found in context.get_files_found():
 		file_found = str(file_found)
 		
 		if os.path.isdir(file_found):
@@ -31,7 +46,6 @@ def get_audiTripdata(files_found, report_folder, seeker, wrap_text, timezone_off
 							time = time.split('Z')[0]
 							timestamp = f'{date} {time}'
 							timestamp = convert_ts_human_to_utc(timestamp)
-							timestamp = convert_utc_human_to_timezone(timestamp, timezone_offset)
 							
 							avgspeed = trip['averageSpeed']
 							tripid = trip['tripID']
@@ -45,23 +59,6 @@ def get_audiTripdata(files_found, report_folder, seeker, wrap_text, timezone_off
 							triptyme = trip['tripType']
 							reportreason = trip['reportReason']
 							
-							data_list.append((timestamp,avgspeed,traveltime,startmileage,mileage,overallmileage,tripid,avgelecconsumpt,avgfuelconsumpt,zeroemdistance,triptyme,reportreason))
-				
-	
-	if len(data_list) > 0:		
-		report = ArtifactHtmlReport('Audi Trip Data')
-		report.start_artifact_report(report_folder, 'Audi Trip Data')
-		report.add_script()
-		data_headers = ('Timestamp','Avg. Speed','Travel Time','Start Mileage','Mileage','Overall Mileage','Trip ID','Avg. Electric Consumption','Avg. Fuel Consumption','Zero Emission Distance','Trip Type','Report Reason')   
-		report.write_artifact_data_table(data_headers, data_list, file_found)
-		report.end_artifact_report()
-		
-		tsvname = 'Audi Trip Data'
-		tsv(report_folder, data_headers, data_list, tsvname)
-
-__artifacts__ = {
-    "auditrip": (
-        "Audi Trip",
-        ('*/Library/Caches/de.audi.myaudimobileassistant/fsCachedData/**'),
-        get_audiTripdata)
-}
+							data_list.append((timestamp,avgspeed,traveltime,startmileage,mileage,overallmileage,tripid,avgelecconsumpt,avgfuelconsumpt,zeroemdistance,triptyme,reportreason, file_found))
+	data_headers = (('Timestamp', 'datetime'),'Avg. Speed','Travel Time','Start Mileage','Mileage','Overall Mileage','Trip ID','Avg. Electric Consumption','Avg. Fuel Consumption','Zero Emission Distance','Trip Type','Report Reason', 'Source File')   
+	return data_headers, data_list, ''
