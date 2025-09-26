d775d05f9c28fd1ca3fad34160ed3e1dfd77920b
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-09-26 10:44:13 -0300

    Update blockedContacts to use Context object

--- scripts/artifacts/blockedContacts.py
+++ scripts/artifacts/blockedContacts.py
@@ -15,9 +15,11 @@ __artifacts_v2__ = {
 }
 
 from scripts.ilapfuncs import artifact_processor, get_file_path, get_plist_file_content
+from scripts.context import Context
 
 @artifact_processor
-def blockedContacts(files_found, report_folder, seeker, wrap_text, timezone_offset):
+def blockedContacts(context:Context):
+    files_found = context.get_files_found()
     source_path = get_file_path(files_found, "com.apple.cmfsyncagent.plist")
     data_list = []
 
