69afb415d2d0c08beffcd9a9bfda1038e8a08640
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-09-26 10:53:46 -0300

    Update bluetoothPairedReg
    Updated to use Context object and get_plist_content for plist parsing.

--- scripts/artifacts/bluetoothPairedReg.py
+++ scripts/artifacts/bluetoothPairedReg.py
@@ -3,30 +3,28 @@ __artifacts_v2__ = {
         "name": "Bluetooth Paired",
         "description": "",
         "author": "@JohnHyla",
-        "version": "0.0.2",
-        "date": "2024-10-21",
+        "creation_date": "2024-10-21",
+        "last_update_date": "2024-10-21",
         "requirements": "none",
         "category": "Bluetooth",
         "notes": "",
-        "paths": ('**/com.apple.MobileBluetooth.devices.plist'),
+        "paths": ('*/com.apple.MobileBluetooth.devices.plist'),
         "output_types": "standard"
     }
 }
 
 
-import plistlib
-import datetime
-
-from scripts.ilapfuncs import artifact_processor, convert_unix_ts_to_str
+from scripts.ilapfuncs import artifact_processor, convert_unix_ts_to_str, get_plist_content
+from scripts.context import Context
 
 
 @artifact_processor
-def get_bluetoothPairedReg(files_found, report_folder, seeker, wrap_text, timezone_offset):
+def get_bluetoothPairedReg(context:Context):
     data_list = []
-    for file_found in files_found:
+    for file_found in context.get_files_found():
         file_found = str(file_found)
         with open(file_found, 'rb') as f:
-            plist = plistlib.load(f)
+            plist = get_plist_content(f)
         if len(plist) > 0:
             for x in plist.items():
                 macaddress = x[0]
@@ -52,8 +50,8 @@ def get_bluetoothPairedReg(files_found, report_folder, seeker, wrap_text, timezo
                 else:
                     defname = ''
 
-                data_list.append((lastseen, macaddress, usernkey, nameu, deviceid, defname))
+                data_list.append((lastseen, macaddress, usernkey, nameu, deviceid, defname, file_found))
 
-        data_headers = ('Last Seen Time', 'MAC Address', 'Name Key', 'Name', 'Device Product ID', 'Default Name' )     
+        data_headers = ('Last Seen Time', 'MAC Address', 'Name Key', 'Name', 'Device Product ID', 'Default Name', 'Source File')     
 
-        return data_headers, data_list, file_found
+        return data_headers, data_list, ''
