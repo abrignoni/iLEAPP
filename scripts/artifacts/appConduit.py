23dd0e3f83ece274eb1820affb563e238e357ac1
Johann POLEWCZYK <johannplw@me.com>
2025-09-27 22:40:21 +0200

    Update appConduit.py - use new device_ids & os_builds data structures

--- scripts/artifacts/appConduit.py
+++ scripts/artifacts/appConduit.py
@@ -1,8 +1,14 @@
+"""
+This module parses AppConduit log files to extract information about device
+interactions between iPhone and other iOS devices (e.g., Apple Watch).
+"""
+
 __artifacts_v2__ = {
     'appConduit': {
         'name': 'App Conduit',
-        'description': 'The AppConduit log file stores information about interactions \
-            between iPhone and other iOS devices, i.e. Apple Watch',
+        'description': 'The AppConduit log file stores information about \
+            interactions between iPhone and other iOS devices, \
+            i.e. Apple Watch',
         'author': '@ydkhatri',
         'creation_date': '2020-08-05',
         'last_update_date': '2025-04-05',
@@ -15,23 +21,31 @@ __artifacts_v2__ = {
     }
 }
 
-
 import pathlib
 import re
-import scripts.builds_ids as builds_ids
 
-from scripts.ilapfuncs import artifact_processor, get_txt_file_content, convert_log_ts_to_utc
+from scripts.ilapfuncs import artifact_processor, get_txt_file_content, \
+    convert_log_ts_to_utc
 
 
 @artifact_processor
-def appConduit(files_found, report_folder, seeker, wrap_text, timezone_offset):
+def appConduit(context):
+    """
+    Parses each log file found and from lines that match a specific regular
+    expression pattern extracts device connection and disconnection events,
+    along with associated device information.
+    """
+
+    files_found = context.get_files_found()
     source_paths = set()
     data_list = []
     device_type_and_info = {}
 
     info = ''
-    reg_filter = (
-        r'(([A-Za-z]+[\s]+([a-zA-Z]+[\s]+[0-9]+)[\s]+([0-9]+\:[0-9]+\:[0-9]+)[\s]+([0-9]{4}))([\s]+[\[\d\]]+[\s]+[\<a-z\>]+[\s]+[\(\w\)]+)[\s\-]+(((.*)(device+\:([\w]+\-[\w]+\-[\w]+\-[\w]+\-[\w]+))(.*)$)))')
+    reg_filter = r'(([A-Za-z]+[\s]+([a-zA-Z]+[\s]+[0-9]+)[\s]+' + \
+        r'([0-9]+\:[0-9]+\:[0-9]+)[\s]+([0-9]{4}))([\s]+[\[\d\]]+[\s]+' + \
+        r'[\<a-z\>]+[\s]+[\(\w\)]+)[\s\-]+(((.*)(device+\:([\w]+\-[\w]+\-' + \
+        r'[\w]+\-[\w]+\-[\w]+))(.*)$)))'
     date_filter = re.compile(reg_filter)
 
     for file_found in files_found:
@@ -58,20 +72,29 @@ def appConduit(files_found, report_folder, seeker, wrap_text, timezone_offset):
                 if 'devicesAreNowConnected' in values:
                     pairing_id = line_match.group(12).split(' ')[3][:-1]
                     device_type = line_match.group(12).split(' ')[4]
-                    device_info = builds_ids.device_id.get(
-                        device_type, device_type)
+                    device_model = context.get_device_model(device_type)
                     os_build = line_match.group(12).split(' ')[7].strip('()')
-                    os_info = builds_ids.OS_build.get(os_build, os_build)
+                    os_version = context.get_os_version(os_build, device_type)
                     device_type_and_info.setdefault(
-                        pairing_id, f'{device_info} - {os_info}')
+                        pairing_id, {'device_type': device_type,
+                                     'device_model': device_model,
+                                     'os_build': os_build,
+                                     'os_version': os_version})
                     info = 'Connected'
-                    data_list.append((dtime_obj, info, device_id, device_type_and_info.get(
-                        pairing_id, ''), file_name))
+                    data_list.append((dtime_obj, info, device_id, pairing_id,
+                                      device_type, device_model, os_build,
+                                      os_version, file_name))
                 if 'devicesAreNoLongerConnected' in values:
                     pairing_id = line_match.group(12).split(' ')[3][:-1]
+                    device_info = device_type_and_info.get(pairing_id, {})
+                    device_type = device_info.get('device_type', '')
+                    device_model = device_info.get('device_model', '')
+                    os_build = device_info.get('os_build', '')
+                    os_version = device_info.get('os_version', '')
                     info = 'Disconnected'
-                    data_list.append((dtime_obj, info, device_id, device_type_and_info.get(
-                        pairing_id, ''), file_name))
+                    data_list.append((dtime_obj, info, device_id, pairing_id,
+                                      device_type, device_model, os_build,
+                                      os_version, file_name))
                 # if 'Resuming because' in values:
                 #     info = 'Resumed'
                 #     data_list.append((dtime_obj,info,device_id,device_type_tmp,file_name))
@@ -83,7 +106,8 @@ def appConduit(files_found, report_folder, seeker, wrap_text, timezone_offset):
                 #     data_list.append((dtime_obj,info,device_id,device_type_tmp,file_name))
 
     data_headers = (('Timestamp', 'datetime'), 'Device interaction',
-                    'Device ID', 'Device type and OS version', 'Log File Name')
+                    'Device ID', 'Pairing ID', 'Device Type', 'Device Model',
+                    'OS Build', 'OS Version', 'Log File Name')
     source_path = ', '.join(source_paths)
 
     return data_headers, data_list, source_path
