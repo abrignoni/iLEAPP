9efb863520f03deea633a3529f7df4d8c85cb4ce
Johann POLEWCZYK <johannplw@me.com>
2025-09-29 07:59:06 +0200

    Update biomeSync.py - Update with new device_ids and os_builds data structures

--- scripts/artifacts/biomeSync.py
+++ scripts/artifacts/biomeSync.py
@@ -1,78 +1,72 @@
+"""
+Extracts and processes device synchronization information from the
+    Biome Device Sync database.
+"""
+
 __artifacts_v2__ = {
-    "get_biomeSync": {
+    "biome_sync": {
         "name": "Biome - Device Syncs",
         "description": "Parses Biome Device Sync records",
         "author": "@JohnHyla",
-        "version": "0.0.2",
-        "date": "2024-10-17",
+        'creation_date': '2023-03-22',
+        'last_update_date': '2025-03-05',
         "requirements": "none",
         "category": "Biome",
         "notes": "",
-        "paths": ('**/Biome/sync/sync.db*'),
-        "output_types": "standard"
+        "paths": ('*/Biome/sync/sync.db*'),
+        "output_types": "standard",
+        'artifact_icon': 'eye'
     }
 }
 
+from scripts.ilapfuncs import artifact_processor, get_file_path, \
+    get_sqlite_db_records, convert_unix_ts_to_utc
 
-from scripts.builds_ids import OS_build
-from scripts.ilapfuncs import open_sqlite_db_readonly, convert_ts_human_to_utc
-from scripts.ilapfuncs import artifact_processor, convert_utc_human_to_timezone
 
 @artifact_processor
-def get_biomeSync(files_found, report_folder, seeker, wrap_text, timezone_offset):
+def biome_sync(context):
+    """
+    Extracts and processes device synchronization information from the
+    'sync.db' SQLite database.
+    """
 
+    files_found = context.get_files_found()
+    source_path = get_file_path(files_found, 'sync.db')
     data_list = []
-    report_file = 'Unknown'
-    for file_found in files_found:
-        file_found = str(file_found)
-        if not file_found.endswith('.db'):
-            continue # Skip all other files
-        report_file = file_found
-        db = open_sqlite_db_readonly(file_found)
 
-        cursor = db.cursor()
-        cursor.execute('''
-        select
-        datetime(last_sync_date,'unixepoch'),
-        device_identifier,
-        name,
-        case platform
-            when 1 then 'iPad'
-            when 2 then 'iPhone'
-            when 4 then 'macOS'
-            when 6 then 'watchOS'
-            else 'Unknown'
-        end,
-        model,
-        case me
-            when 0 then ''
-            when 1 then 'Yes'
-        end as 'Local Device'
-        from DevicePeer
-        ''')
+    query = '''
+        SELECT
+            last_sync_date,
+            device_identifier,
+            name,
+            CASE platform
+                WHEN 1 THEN 'iPad'
+                WHEN 2 THEN 'iPhone'
+                WHEN 3 THEN 'Mac'
+                WHEN 4 THEN 'Mac'
+                WHEN 5 THEN 'AppleTV'
+                WHEN 6 THEN 'Watch'
+                WHEN 7 THEN 'AppleTV'
+                ELSE 'Unknown'
+            END,
+            model,
+            CASE me
+                WHEN 0 THEN ''
+                WHEN 1 THEN 'Yes'
+            END AS 'Local Device'
+        FROM DevicePeer
+    '''
 
-        all_rows = cursor.fetchall()
+    data_headers = (('Last Sync Timestamp', 'datetime'), 'Device ID', 'Name',
+                    'Device Type', 'OS Build', 'OS Version', 'Local Device')
 
-        for row in all_rows:
-            if row[0] == ' ':
-                timestamp = ''
-            elif row[0] is None:
-                timestamp = row[0]
-            else:
-                timestamp = row[0]
-                timestamp = convert_ts_human_to_utc(timestamp)
-                timestamp = convert_utc_human_to_timezone(timestamp, timezone_offset)
+    db_records = get_sqlite_db_records(source_path, query)
 
-            os_build = 'Unknown'
-            for key, value in OS_build.items():
-                if str(row[4]) == key:
-                    os_build = value
-                    break
+    for record in db_records:
+        timestamp = convert_unix_ts_to_utc(record[0])
+        os_version = context.get_os_version(record[4], record[3])
 
-            data_list.append((timestamp, row[1], row[2], row[3], row[4], os_build, row[5]))
+        data_list.append((timestamp, record[1], record[2], record[3],
+                          record[4], os_version, record[5]))
 
-        db.close()
-
-    data_headers = (('Last Sync Timestamp', 'datetime'), 'Device ID', 'Name', 'Device Type', 'OS Build', 'OS Version', 'Local Device')
-
-    return data_headers, data_list, report_file
+    return data_headers, data_list, source_path
