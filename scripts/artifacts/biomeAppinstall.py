a6b28ff11b1847c1922d7f1ab59c2a7d6cc0e566
Gear-I <dielleaiko@hotmail.com>
2026-06-18 18:22:11 -0400

    Fix biomeAppinstall protobuf decode crash

--- scripts/artifacts/biomeAppinstall.py
+++ scripts/artifacts/biomeAppinstall.py
@@ -4,7 +4,7 @@ __artifacts_v2__ = {
         "description": "Parses airplane mode entries from biomes",
         "author": "@JohnHyla",
         "creation_date": "2024-10-17",
-        "last_update_date": "2025-10-31",
+        "last_update_date": "2026-06-18",
         "requirements": "none",
         "category": "Biome",
         "notes": "",
@@ -19,6 +19,7 @@ import blackboxprotobuf
 from scripts.ccl_segb.ccl_segb import read_segb_file
 from scripts.ccl_segb.ccl_segb_common import EntryState
 from scripts.ilapfuncs import artifact_processor, webkit_timestampsconv
+from scripts.ilapfuncs import artifact_processor, logfunc
 
 @artifact_processor
 def get_biomeAppinstall(context):
@@ -90,52 +91,98 @@ def get_biomeAppinstall(context):
     }
 
     data_list = []
+
     for file_found in context.get_files_found():
         file_found = str(file_found)
         filename = os.path.basename(file_found)
+
         if filename.startswith('.'):
             continue
-        if os.path.isfile(file_found):
-            if 'tombstone' in file_found:
-                continue
-        else:
+
+        if not os.path.isfile(file_found):
             continue
-    
+
+        if 'tombstone' in file_found:
+            continue
+
         for record in read_segb_file(file_found):
             ts = record.timestamp1
             ts = ts.replace(tzinfo=timezone.utc)
 
             if record.state == EntryState.Written:
+                try:
+                    protostuff, _ = blackboxprotobuf.decode_message(record.data, typess)
+                except (KeyError, ValueError, TypeError, IndexError) as ex:
+                    logfunc(f"Skipping biomeAppinstall record due to protobuf decode error: {ex}")
+                    continue
 
-                protostuff, _ = blackboxprotobuf.decode_message(record.data, typess)
-            
-                activity = (protostuff['1']['1'])
-                timestart = (webkit_timestampsconv(protostuff['2']))
-                timeend = (webkit_timestampsconv(protostuff['3']))
-                
-                bundleid = (protostuff['4']['3'])
-                actionguid = (protostuff['5'])
+                activity = protostuff['1']['1']
+                timestart = webkit_timestampsconv(protostuff['2'])
+                timeend = webkit_timestampsconv(protostuff['3'])
+
+                bundleid = protostuff['4']['3']
+                actionguid = protostuff['5']
                 appinfo1 = appinfo2 = bundleinfo = ''
+
                 if protostuff.get('7', '') != '':
                     if isinstance(protostuff['7'], list):
                         if len(protostuff['7']) < 3:
-                            appinfo1 = (protostuff['7'][0]['2'].get('3', ''))
+                            appinfo1 = protostuff['7'][0]['2'].get('3', '')
                         else:
-                            appinfo1 = (protostuff['7'][0]['2'].get('3', ''))
-                            bundleinfo = (protostuff['7'][1]['2'].get('3', ''))
-                            appinfo2 = (protostuff['7'][2]['2'].get('3', ''))
-                
-                timewrite = (webkit_timestampsconv(protostuff['8']))
-                
-                data_list.append((ts, timestart, timeend, timewrite, record.state.name, activity, bundleid, bundleinfo,
-                                  appinfo1, appinfo2, actionguid, filename, record.data_start_offset))
+                            appinfo1 = protostuff['7'][0]['2'].get('3', '')
+                            bundleinfo = protostuff['7'][1]['2'].get('3', '')
+                            appinfo2 = protostuff['7'][2]['2'].get('3', '')
+
+                timewrite = webkit_timestampsconv(protostuff['8'])
+
+                data_list.append((
+                    ts,
+                    timestart,
+                    timeend,
+                    timewrite,
+                    record.state.name,
+                    activity,
+                    bundleid,
+                    bundleinfo,
+                    appinfo1,
+                    appinfo2,
+                    actionguid,
+                    filename,
+                    record.data_start_offset
+                ))
 
             elif record.state == EntryState.Deleted:
-                data_list.append((ts, None, None, None, record.state.name, None, None, None, None, None, None, filename,
-                                  record.data_start_offset))
+                data_list.append((
+                    ts,
+                    None,
+                    None,
+                    None,
+                    record.state.name,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    filename,
+                    record.data_start_offset
+                ))
 
-
-    data_headers = (('SEGB Timestamp', 'datetime'), ('Timestamp', 'datetime'), ('Time End', 'datetime'), ('Time Write', 'datetime'), 'SEGB State', 'Activity', 'Bundle ID', 'Bundle Info', 'App Info', 'App Info2', 'Action GUID', 'Filename', 'Offset')
+    data_headers = (
+        ('SEGB Timestamp', 'datetime'),
+        ('Timestamp', 'datetime'),
+        ('Time End', 'datetime'),
+        ('Time Write', 'datetime'),
+        'SEGB State',
+        'Activity',
+        'Bundle ID',
+        'Bundle Info',
+        'App Info',
+        'App Info2',
+        'Action GUID',
+        'Filename',
+        'Offset'
+    )
 
     return data_headers, data_list, 'see Filename for more info'
 
