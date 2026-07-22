56ab194bde8f45685fdaf1d2d73c70df3aa6eba6
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2026-07-21 23:06:45 -0400

    More Timeline Updates

--- scripts/artifacts/Ph080comappleMobileSlideShowPlist.py
+++ scripts/artifacts/Ph080comappleMobileSlideShowPlist.py
@@ -1,37 +1,37 @@
 __artifacts_v2__ = {
-'Ph080ComAppleMobileSlideshowPlist': {
-'name': 'Ph080-Com-Apple-MobileSlideshow-Plist',
-'description': 'Parses basic data from com.apple.mobileslideshow.plist which contains some important'
-' data related to the Apple Photos Application. Additional information and explanation of some'
-' keys-fields might be found with research and published blogs written by Scott Koenig'
-' https://theforensicscooter.com/2024/05/18/ileapp-parsers-photos-sqlite-queries/',
-'author': 'Scott Koenig',
-'version': '5.0',
-'date': '2025-01-05',
-'requirements': 'Acquisition that contains com.apple.mobileslideshow.plist',
-'category': 'Photos.sqlite-Y-Settings-Plist-MobileSlideShow',
-'notes': '',
-'paths': ('*/Library/Preferences/com.apple.mobileslideshow.plist',),
-"output_types": ["standard", "tsv", "none"],
-"artifact_icon": "settings",
-'sample_data': {
-'ctf2020_ios12': 'iOS 12.4 | 23 rows',
-'dexter_ios18': 'iOS 18.3.2 | 64 rows',
-'felix_ios17': 'iOS 17.6.1 | 26 rows',
-'fsfull002_ios17': 'iOS 17.1 | 8 rows',
-'hc_ios18_7': 'iOS 18.7.8 | 6 rows',
-'iphone11_ios17': 'iOS 17.3 | 19 rows',
-'iphone12_ios18': 'iOS 18.7 | 35 rows',
-'iphone14plus_ios18': 'iOS 18.0 | 5 rows',
-'otto_ios17': 'iOS 17.5.1 | 26 rows',
-'abe_ios16': 'iOS 16.5 | 27 rows',
-'felix23_ios16': 'iOS 16.5 | 23 rows',
-'hickman_ios13': 'iOS 13.3.1 | 26 rows',
-'hickman_ios14': 'iOS 14.3 | 22 rows',
-'jess_ios15': 'iOS 15.0.2 | 6 rows',
-'magnet_ios16': 'iOS 16.1.1 | 6 rows',
-}
-}
+    "Ph080ComAppleMobileSlideshowPlist": {
+        "name": "Ph080-Com-Apple-MobileSlideshow-Plist",
+        "description": "Parses basic data from com.apple.mobileslideshow.plist which contains some important"
+        " data related to the Apple Photos Application. Additional information and explanation of some"
+        " keys-fields might be found with research and published blogs written by Scott Koenig"
+        " https://theforensicscooter.com/2024/05/18/ileapp-parsers-photos-sqlite-queries/",
+        "author": "Scott Koenig",
+        "creation_date": "2025-01-05",
+        "last_update_date": "2026-07-21",
+        "requirements": "Acquisition that contains com.apple.mobileslideshow.plist",
+        "category": "Photos.sqlite-Y-Settings-Plist-MobileSlideShow",
+        "notes": "",
+        "paths": ('*/Library/Preferences/com.apple.mobileslideshow.plist',),
+        "output_types": ["html", "tsv", "lava"],
+        "artifact_icon": "settings",
+        "sample_data": {
+            "ctf2020_ios12": "iOS 12.4 | 23 rows",
+            "dexter_ios18": "iOS 18.3.2 | 64 rows",
+            "felix_ios17": "iOS 17.6.1 | 26 rows",
+            "fsfull002_ios17": "iOS 17.1 | 8 rows",
+            "hc_ios18_7": "iOS 18.7.8 | 6 rows",
+            "iphone11_ios17": "iOS 17.3 | 19 rows",
+            "iphone12_ios18": "iOS 18.7 | 35 rows",
+            "iphone14plus_ios18": "iOS 18.0 | 5 rows",
+            "otto_ios17": "iOS 17.5.1 | 26 rows",
+            "abe_ios16": "iOS 16.5 | 27 rows",
+            "felix23_ios16": "iOS 16.5 | 23 rows",
+            "hickman_ios13": "iOS 13.3.1 | 26 rows",
+            "hickman_ios14": "iOS 14.3 | 22 rows",
+            "jess_ios15": "iOS 15.0.2 | 6 rows",
+            "magnet_ios16": "iOS 16.1.1 | 6 rows",
+        }
+    }
 }
 
 import os
