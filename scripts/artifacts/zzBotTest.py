6ec3fc0d2ffce7558c6800769ff1cb3c3c099e5e
Brigs <abrignoni@gmail.com>
2026-08-24 16:06:34 -0500

    Add placeholder artifact for the bot end-to-end test

---
+++ scripts/artifacts/zzBotTest.py
@@ -0,0 +1,21 @@
+__artifacts_v2__ = {
+    "zzBotTest": {
+        "name": "Bot Test Placeholder",
+        "description": "Temporary placeholder used to exercise the test-data bot end to end. This PR is never merged.",
+        "author": "@AlexisBrignoni",
+        "creation_date": "2026-08-24",
+        "last_update_date": "2026-08-24",
+        "requirements": "none",
+        "category": "Identifiers",
+        "paths": ('*/zz_bot_test_placeholder.plist',),
+        "output_types": "none",
+        "artifact_icon": "device-mobile"
+    }
+}
+
+from scripts.ilapfuncs import artifact_processor
+
+
+@artifact_processor
+def zzBotTest(context):
+    return (), [], ''
